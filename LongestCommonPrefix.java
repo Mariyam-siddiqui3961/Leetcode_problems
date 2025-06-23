99a2a4230bd042fa311a3f2b7bc8ab4f3f8bafbf
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-06-23 16:15:57 +0000

    container config done

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -6,3 +6,8 @@ class Solution {
             for ( int j=1; j< strs.length; j++){
                 if (i >= strs[j].length() || strs[j].charAt(i) !=c)
                     return strs[0].substring(0,i);
+                    }
+        }
+        return strs[0];
+    }
+}

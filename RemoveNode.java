16a4946ad8502bec1432defaf8fa510c0ff9da05
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-06-25 16:19:38 +0000

    container config done

--- RemoveNode.java
+++ RemoveNode.java
@@ -1,1 +1,23 @@
 //--------------Remove Nth node from end of linked list-----------------------
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+          ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+        
+        
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }

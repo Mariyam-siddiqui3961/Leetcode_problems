739aade09ab926a9e7bc1b117b243021bbece01b
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-06-27 16:22:13 +0000

    container config done

--- ValidParanthesis.java
+++ ValidParanthesis.java
@@ -1,1 +1,11 @@
 //-------------------Valid Parantheses-------------------------
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) return false;
+                

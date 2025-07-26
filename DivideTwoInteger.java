7dcd8ecf851c21f4c09a235972e7473ea8d90176
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-07-26 14:13:45 +0000

    container config done

--- DivideTwoInteger.java
+++ DivideTwoInteger.java
@@ -1,1 +1,9 @@
 //---------------Divide Two Integer -----------------------
+public class Solution {
+    public int divide(int dividend, int divisor) {
+        
+        if (dividend == Integer.MIN_VALUE && divisor == -1)
+            return Integer.MAX_VALUE;
+
+        
+        boolean negative = (dividend < 0) ^ (divisor < 0);

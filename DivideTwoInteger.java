e7271ef4f7f0ffba35449666219f0e7333bf4060
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-07-26 14:14:22 +0000

    container config done

--- DivideTwoInteger.java
+++ DivideTwoInteger.java
@@ -7,3 +7,24 @@ public class Solution {
 
         
         boolean negative = (dividend < 0) ^ (divisor < 0);
+        long ldividend = Math.abs((long) dividend);
+        long ldivisor = Math.abs((long) divisor);
+
+        int quotient = 0;
+
+        while (ldividend >= ldivisor) {
+            long temp = ldivisor, multiple = 1;
+            
+            
+            while (ldividend >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+
+            ldividend -= temp;
+            quotient += multiple;
+        }
+
+        return negative ? -quotient : quotient;
+    }
+}

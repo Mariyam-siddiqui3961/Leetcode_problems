301902721e16c769ed70cdab92f70ea5c9d729d1
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-07-28 01:51:47 +0000

    container config done

--- PatchingArray.java
+++ PatchingArray.java
@@ -3,3 +3,8 @@ class Solution {
     public int minPatches(int[] nums, int n) {
         long miss =1;
         int i=0, patches=0;
+         while(miss<=n){
+            if(i<nums.length && nums[i] <= miss){
+                miss+=nums[i];
+                i++;
+            }else{

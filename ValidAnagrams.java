89784d5df09d44a669782fe2d4266353a45eccc8
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-07-29 10:16:09 +0000

    container config done

--- ValidAnagrams.java
+++ ValidAnagrams.java
@@ -1,1 +1,5 @@
 //---------------------VAlid Anagrams ---------------------
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+        return false;

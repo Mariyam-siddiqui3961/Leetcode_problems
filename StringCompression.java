ef61ce2a394188d8d554fe52fce31067dfa0c318
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-07-30 06:43:18 +0000

    container config done

--- StringCompression.java
+++ StringCompression.java
@@ -1,1 +1,5 @@
 //------------------String COmpression---------------------
+class Solution {
+    public int compress(char[] chars) {
+        int write = 0; // index to write in chars
+        int anchor = 0; // start index of current group

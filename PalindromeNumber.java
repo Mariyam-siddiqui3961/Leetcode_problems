aa1ca13f0a4a8a060f5f37a7ffb334d28523a698
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-01 08:51:56 +0000

    container config done

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -8,4 +8,16 @@ class Solution {
         //     x = x / 10;
         // }
         // return x == reversed || x == reversed / 10;
-        
+        String str = Integer.toString(x);
+        int i=0;
+        int j=str.length()-1;
+        while(i<j){
+             if(str.charAt(i)!=str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}

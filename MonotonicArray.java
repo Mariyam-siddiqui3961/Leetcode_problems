8bb6de8f4d3f8014d731d75ba332a9d54d40955f
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-04 12:09:30 +0000

    container config done

--- MonotonicArray.java
+++ MonotonicArray.java
@@ -2,3 +2,7 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
         boolean increasing = true;
+         boolean decreasing = true;
+
+        for (int i=1; i< nums.length; i++){
+            if (nums[i]<nums[i-1]){

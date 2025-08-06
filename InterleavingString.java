bee5a90ef5fa1cd5ffdb997b77c0df5158028336
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-06 13:51:33 +0000

    container config done

--- InterleavingString.java
+++ InterleavingString.java
@@ -3,3 +3,5 @@ class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         int m = s1.length();
         int n = s2.length();
+        //length must be add up
+        if (m+n!= s3.length()) return false;

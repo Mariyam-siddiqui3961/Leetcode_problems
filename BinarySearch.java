0a57d06331520360cbc0eac8fb5da319d8dc8eaa
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-07 13:00:20 +0000

    container config done

--- BinarySearch.java
+++ BinarySearch.java
@@ -3,4 +3,6 @@ class Solution {
     public int search(int[] nums, int target) {
         int low= 0;
         int high = nums.length-1;
-        
+        while(low<=high){
+            int mid = low+ (high-low)/2;
+            

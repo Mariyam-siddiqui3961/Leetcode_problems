90f9e84b736804778048da2931f8d0aee1a0cf64
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-08 08:37:50 +0000

    container config done

--- FirstAndLastPosition.java
+++ FirstAndLastPosition.java
@@ -10,3 +10,9 @@ class Solution {
         while(low<=high){
             int mid = (low+high)/2;
             if (nums[mid]==target){
+                 result=mid;
+                high= mid-1;
+            }else if (nums[mid]<target){
+                low= mid+1;
+            }else {
+                high=mid-1;

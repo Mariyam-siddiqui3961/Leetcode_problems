0d19c6711074b14b0a9cd609557887e16f516dcd
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-08 08:36:53 +0000

    container config done

--- FirstAndLastPosition.java
+++ FirstAndLastPosition.java
@@ -1,1 +1,7 @@
 //---------------First and Last position (Binary Search Application)-------------------
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums , target);
+        return new int [] {first, last};
+    }

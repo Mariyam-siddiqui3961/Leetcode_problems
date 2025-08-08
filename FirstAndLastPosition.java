0f043b280033361d3c3310fedc0dbb105aa9a638
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-08 08:39:09 +0000

    container config done

--- FirstAndLastPosition.java
+++ FirstAndLastPosition.java
@@ -20,5 +20,12 @@ class Solution {
         }
         return result;
     }
-    
+     private int findLast(int[]nums, int target){
+        int low=0, high= nums.length-1, result=-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if (nums[mid]==target){
+                result=mid;
+                low = mid +1;
+
 

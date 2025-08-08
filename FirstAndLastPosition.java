984a91f0a210d4be955b63ec82368dfcadd938b5
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-08 08:37:21 +0000

    container config done

--- FirstAndLastPosition.java
+++ FirstAndLastPosition.java
@@ -5,3 +5,8 @@ class Solution {
         int last = findLast(nums , target);
         return new int [] {first, last};
     }
+    private int findFirst(int[] nums , int target){
+        int low = 0, high= nums.length-1, result=-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if (nums[mid]==target){

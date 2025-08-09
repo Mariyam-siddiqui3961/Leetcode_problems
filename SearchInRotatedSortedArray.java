b46a035a2558c20fadf7977f5abae0a3426163a6
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-09 07:07:08 +0000

    container config done

--- SearchInRotatedSortedArray.java
+++ SearchInRotatedSortedArray.java
@@ -1,1 +1,5 @@
 //--------------Search in Rotated Sorted Array-------------------
+class Solution {
+    public int search(int[] nums, int target) {
+        int low =0, high=nums.length-1;
+        

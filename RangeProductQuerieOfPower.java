52ad6ee96ae8991facf08dd2b600a7fdadad76c1
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-11 16:03:50 +0000

    container config done

--- RangeProductQuerieOfPower.java
+++ RangeProductQuerieOfPower.java
@@ -1,1 +1,5 @@
 //-------------Range of Product Queries of java-------------------------
+class Solution {
+    public int[] productQueries(int n, int[][] queries) {
+         final int MOD = 1_000_000_007;
+        // Step 1: Extract powers of 2 from n

f61016b368e87a6b37f4e21e03970a88982000d3
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-11 16:04:23 +0000

    container config done

--- RangeProductQuerieOfPower.java
+++ RangeProductQuerieOfPower.java
@@ -3,3 +3,13 @@ class Solution {
     public int[] productQueries(int n, int[][] queries) {
          final int MOD = 1_000_000_007;
         // Step 1: Extract powers of 2 from n
+        List<Integer> powers = new ArrayList<>();
+        int bit = 0;
+        int temp = n;
+        while (temp > 0) {
+            if ((temp & 1) == 1) {
+                powers.add(1 << bit);
+            }
+            bit++;
+            temp >>= 1;
+        }

44d6ae8d2d84e6ba16e4c5ac5b17198be9792f87
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-12 16:24:03 +0000

    container config done

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -1,3 +1,5 @@
 //-------------------Moves Zeroes--------------------------------
 class Solution {
     public void moveZeroes(int[] nums) {
+         int left = 0; 
+        for(int right=0; right<nums.length; right++){

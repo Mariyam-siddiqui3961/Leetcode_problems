20d4456f5c69369fefc3a570dc9747a1b94ef022
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-13 14:45:47 +0000

    container config done

--- RotateImage.java
+++ RotateImage.java
@@ -8,3 +8,6 @@ class Solution {
                 matrix[i][j]= matrix[j][i];
                 matrix[j][i]= temp;
             }
+            }
+        for (int i= 0; i<n; i++){
+            for (int j=0; j<n/2; j++){

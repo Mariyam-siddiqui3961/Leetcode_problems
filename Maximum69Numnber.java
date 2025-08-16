26d6f14d9fb9f83e51c0d874f5ffeeb1d09cf9cb
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-16 08:01:48 +0000

    container config done

--- Maximum69Numnber.java
+++ Maximum69Numnber.java
@@ -1,3 +1,6 @@
 //------------------Maximum 69 NUmber ------------------------------
 class Solution {
     public int maximum69Number (int num) {
+        char[] digits = String.valueOf(num).toCharArray();
+        for (int i = 0; i < digits.length; i++) {
+            

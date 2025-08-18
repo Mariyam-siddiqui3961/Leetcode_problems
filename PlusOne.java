85aea81d972cdb64325a5ed4da0960909a7e4d60
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-18 16:35:03 +0000

    ccontainer config done

--- PlusOne.java
+++ PlusOne.java
@@ -1,1 +1,11 @@
 //------------------------Plus one------------------------
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int i = digits.length - 1;
+        
+        // Traverse the array from the last digit to the first
+        for (i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0; // Set current digit to 0 if it's 9
+            } else {
+                digits[i]++; // Increment the current digit

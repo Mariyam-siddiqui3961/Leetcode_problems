9b4706cc7875fd4d6d0696c452ba28d9c27091d4
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-18 16:35:38 +0000

    container config done

--- PlusOne.java
+++ PlusOne.java
@@ -9,3 +9,14 @@ class Solution {
                 digits[i] = 0; // Set current digit to 0 if it's 9
             } else {
                 digits[i]++; // Increment the current digit
+                return digits; // Return the result if no carry is left
+            }
+        }
+        
+        // If we exit the loop, it means we have a carry that needs to be added
+        // Create a new array with an extra space for the carry
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1; // Set the first digit to 1
+        return newDigits; // Return the new array
+    }
+}

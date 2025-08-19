2b55ee968faba374d728348d16436099c18cdc13
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-19 14:53:46 +0000

    container config done

--- SingleNumber.java
+++ SingleNumber.java
@@ -1,1 +1,7 @@
 //------------Single Number--------------
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+    
+        for( int i=0; i < nums.length; i++ ){
+            result ^= nums[i];

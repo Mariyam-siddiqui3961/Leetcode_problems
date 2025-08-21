b60f022b7ff1061fd8b03df8ff124316589da369
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-21 14:06:49 +0000

    container config done

--- TwoSumII.java
+++ TwoSumII.java
@@ -2,3 +2,7 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int left =0, right=numbers.length-1;
+        while (left<right){
+            int sum= numbers[left] +numbers[right];
+            if (sum==target){
+                return new int[]{left+1, right+1};

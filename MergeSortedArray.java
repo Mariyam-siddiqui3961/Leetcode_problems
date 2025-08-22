e9ff9f5b77a7c13d6c52b23434702aa22a26438b
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-22 14:47:43 +0000

    container config done

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -7,3 +7,7 @@ class Solution {
 
         while ( i>=0 && j>=0){
             if (nums1[i]> nums2[j]){
+                 nums1[k--]=nums1[i--];
+            }
+            else {
+                nums1[k--]= nums2[j--];

038de6bae068761f136fc2c410cdb2304355e5f4
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-23 13:33:03 +0000

    container config done

--- SingleElementSortedArray.java
+++ SingleElementSortedArray.java
@@ -1,1 +1,4 @@
 //-----------------Single Element in a Sorted Array--------------------
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int low = 0, high = nums.length-1;

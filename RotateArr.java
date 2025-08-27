a69dfcdc85199b1de112058295e79d8c8cd0cc58
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-27 17:50:33 +0000

    container config done

--- RotateArr.java
+++ RotateArr.java
@@ -2,3 +2,5 @@
 public void rotate(int[] nums, int k) {
     int n = nums.length;
     k = k % n;   // handle if k > n
+    for (int r = 0; r < k; r++) {   // repeat k times (outer loop)
+        int last = nums[n - 1];     // save the last element

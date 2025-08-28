f8cf13e6e56523249caba181fd3b3e2a0d505988
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-28 18:05:16 +0000

    container config done

--- ReverseVowelsOfString.java
+++ ReverseVowelsOfString.java
@@ -2,3 +2,6 @@
 class Solution {
     public String reverseVowels(String s) {
         char[] chars = s.toCharArray();
+        int left = 0;
+        int right = s.length() -1;
+        String vowels = "aeiouAEIOU";

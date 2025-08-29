8524317cf98b519edb05efcf15ff2355916f948f
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-29 15:34:23 +0000

    container config done

--- ValidPalindromeTwo.java
+++ ValidPalindromeTwo.java
@@ -1,3 +1,6 @@
 //----------------Valid Palindrome -----------------
 class Solution {
     public boolean isPalindrome(String str) {
+         str = str.toLowerCase().replaceAll("[^a-z0-9]", "");
+        
+        int n = str.length();

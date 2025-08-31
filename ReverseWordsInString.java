d74f47cbba3cf787733b36aa35c9e74fcf077469
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-08-31 15:33:21 +0000

    container config done

--- ReverseWordsInString.java
+++ ReverseWordsInString.java
@@ -1,1 +1,5 @@
 //-----------------Reverse Words in a String --------------------
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+        StringBuilder sb = new StringBuilder();

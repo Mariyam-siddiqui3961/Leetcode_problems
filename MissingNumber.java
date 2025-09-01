0fdb6b2b6ab59319ec55868d2beee81ba8a3f8a3
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-01 15:14:02 +0000

    container config done

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,3 +1,7 @@
 //------------Missing Number-----------------
 class Solution {
     public int missingNumber(int[] nums) {
+        int n = nums.length;
+
+        int expectedSum = n *(n+1)/2;
+        int actualSum = 0;

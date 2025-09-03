cdd43c544102ed6e90ceeb1d35137f12c34693aa
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-03 13:44:51 +0000

    container config done

--- TwoSumII.java
+++ TwoSumII.java
@@ -22,3 +22,7 @@ int sum = 0;
         for (int i=0; i<nums.length; i++){
             for (int j = i+1; j<nums.length; j++){
                 if(nums[i]+nums[j]==target){
+                    return new int []{i,j};
+                }
+            }
+        }

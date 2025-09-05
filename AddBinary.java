1cf222f7c90cfd211339e17c8e3a7e69b55fd543
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-05 16:05:16 +0000

    container config done

--- AddBinary.java
+++ AddBinary.java
@@ -2,3 +2,6 @@
 class Solution {
     public String addBinary(String a, String b) {
         StringBuilder result = new StringBuilder();
+        int i= a.length()-1;
+        int j= b.length()-1;
+        int carry=0;

e9d0afb58e2cb4ffca5fb7306bc849310ef3d489
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-06 05:01:52 +0000

    container config done

--- ValidAnagram.java
+++ ValidAnagram.java
@@ -1,4 +1,10 @@
 //-------------------Valid Anagram------------------
 class Solution {
     public boolean isAnagram(String s, String t) {
-        
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        // Step 1: Count chars in s
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);

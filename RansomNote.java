ff03bc47790837f74e5ade90ed3dabc8ddf71575
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-07 16:28:47 +0000

    container config done

--- RansomNote.java
+++ RansomNote.java
@@ -1,1 +1,5 @@
 //-------------Ransom Note-------------------
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        
+         HashMap<Character, Integer> map = new HashMap<>();

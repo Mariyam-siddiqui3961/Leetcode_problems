1cdfb28eea8a2b899b70e9f7e9d79aaa96fc75e1
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-07 16:29:53 +0000

    container config done

--- RansomNote.java
+++ RansomNote.java
@@ -7,3 +7,8 @@ class Solution {
         for (char c : magazine.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
+        // Check ransomNote
+        for (char c : ransomNote.toCharArray()) {
+            if (!map.containsKey(c) || map.get(c) == 0) {
+                return false;  // Not enough of this character
+            }

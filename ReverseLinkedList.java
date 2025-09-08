19b80b782207695863ebee141ec501a73b7f2383
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-08 12:47:04 +0000

    container config done

--- ReverseLinkedList.java
+++ ReverseLinkedList.java
@@ -1,12 +1,16 @@
 //-----------Reverse Linked List---------------------
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         if ( head == null || head.next==null){
+//             return head;
+//             }
+//         ListNode reversedList = reverseList(head.next);
+//         head.next.next = head;
+//         head.next= null;
+//          return reversedList;
+//     }
+// }
+
+
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if ( head == null || head.next==null){
-            return head;
-            }
-        ListNode reversedList = reverseList(head.next);
-        head.next.next = head;
-        head.next= null;
-         return reversedList;
-    }
-}

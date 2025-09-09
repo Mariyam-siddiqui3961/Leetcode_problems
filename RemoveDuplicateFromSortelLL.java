04dbcc9c46232e39f18a8e9929b6cc41d3c01bb5
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-09 16:53:18 +0000

    container config done

--- RemoveDuplicateFromSortelLL.java
+++ RemoveDuplicateFromSortelLL.java
@@ -8,3 +8,9 @@ class Solution {
             if(current.val == current.next.val){
                  current.next = current.next.next;
             }else{
+                current =current.next;
+            }
+        }
+        return head;
+    }
+}

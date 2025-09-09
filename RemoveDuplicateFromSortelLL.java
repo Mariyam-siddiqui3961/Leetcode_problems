7532dbf4fdaa2aa3a7fcab2c1b0b32bb36bcd256
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-09 16:51:53 +0000

    container config done

--- RemoveDuplicateFromSortelLL.java
+++ RemoveDuplicateFromSortelLL.java
@@ -1,1 +1,4 @@
 //--------------Remove Duplicate From Sorted Linked List----------------------
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;

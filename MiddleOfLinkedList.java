48ec97f477ff8a1e032ab8fbe084fb1f1163fd2c
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-10 14:41:25 +0000

    container config done

--- MiddleOfLinkedList.java
+++ MiddleOfLinkedList.java
@@ -5,3 +5,9 @@ class Solution {
         ListNode fast = head;
          while(fast !=null && fast.next !=null){
             slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+}

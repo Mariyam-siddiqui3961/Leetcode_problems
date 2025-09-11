b2f3b446a059e0522ee1e391e9ca461709ca4174
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-11 10:47:54 +0000

    container config done

--- SortVowelInString.java
+++ SortVowelInString.java
@@ -2,4 +2,9 @@
 class Solution {
     public String sortVowels(String s) {
         // Helper methos to check vowels
+        java.util.Set<Character> vowels = new java.util.HashSet<>();
+        for (char c : "aeiouAEIOU".toCharArray()){
+            vowels.add(c);
+        }
+        // Collect vowels
         

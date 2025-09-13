c9c097a58030441f2a3e42c27aaa8a905724ac26
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-13 17:34:07 +0000

    container config done

--- LongestSubstring.java
+++ LongestSubstring.java
@@ -5,3 +5,6 @@ class Solution {
          int left = 0, max =0;
         for (int right=0; right<s.length(); right++){
             while (set.contains(s.charAt(right))){
+                set.remove(s.charAt(left++));
+            }
+            set.add(s.charAt(right));

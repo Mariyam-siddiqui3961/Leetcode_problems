4c4dccaaad841323bf10e009ab229ba780144303
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-15 15:37:51 +0000

    container config done

--- PartitionArray.java
+++ PartitionArray.java
@@ -2,3 +2,8 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
         int n = nums.length;
+        int[] result = new int[n];
+        int index = 0;
+
+        for (int num: nums){
+            if(num<pivot){

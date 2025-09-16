d1acb0a58f495386bdc2eab2d9e65c7149e16703
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-16 17:24:15 +0000

    container config done

--- RotateImage.java
+++ RotateImage.java
@@ -7,3 +7,15 @@ class Solution {
                 int temp= matrix[i][j];
                 matrix[i][j]= matrix[j][i];
                 matrix[j][i]= temp;
+                 }
+        }
+        for (int i= 0; i<n; i++){
+            for (int j=0; j<n/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j]= matrix[i][n-1-j];
+                matrix[i][n-1-j]= temp;
+            }
+
+        }
+    }
+}

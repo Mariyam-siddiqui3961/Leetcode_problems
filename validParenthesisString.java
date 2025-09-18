8f1821fe9f47b13e581b5c7eb598bc2972e43a75
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-18 11:15:13 +0000

    container config done

--- validParenthesisString.java
+++ validParenthesisString.java
@@ -2,4 +2,14 @@
 class Solution {
     public boolean checkValidString(String s) {
         int low = 0;   
-        int high = 0;  
+        int high = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                low++;
+                high++;
+            } else if (c == ')') {
+                low--;
+                high--;
+            } else { 
+                low--;     
+                high++; 

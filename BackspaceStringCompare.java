ef149c6b3353b433ce0dc99d5a47e73c275aac5c
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-22 06:32:32 +0000

    container config done

--- BackspaceStringCompare.java
+++ BackspaceStringCompare.java
@@ -14,3 +14,24 @@ class Solution {
                 }else if (skipS > 0){
                     skipS--; i--;
                 }else break;
+                }
+            while(j>=0){
+                if(t.charAt(j) == '#'){
+                    skipT++; j--;
+                }else if(skipT>0){
+                    skipT--; j--;
+                }else break;
+            }
+
+            if(i>=0 && j>=0){
+                if(s.charAt(i) != t.charAt(j)) return false;
+            }else{
+                if(i >=0 || j>=0) return false;
+            }
+            i--;
+            j--;
+
+        }
+        return true;
+    }
+}

0135d1f58ff8c48238a384cab4ab06da2d5c6da0
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-23 08:00:20 +0000

    container config done

--- generateParenthesis.java
+++ generateParenthesis.java
@@ -1,1 +1,7 @@
 //--------------Generate Parenthesis--------------
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+
+        generate(result, "", 0, 0, n);
+        return result;

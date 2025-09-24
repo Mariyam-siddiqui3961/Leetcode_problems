8c56f3ef6d9b582161c9d70cc422521386eb6401
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-24 15:27:31 +0000

    container config done

--- FractionToRecurringDecimal.java
+++ FractionToRecurringDecimal.java
@@ -1,1 +1,6 @@
 //----------------Fraction To Recurring Decimal-----------------
+class Solution {
+    public String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) return "0";
+
+        StringBuilder sb = new StringBuilder();

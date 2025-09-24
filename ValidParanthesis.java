cd41bf3518f09734b167fbbdb44e1b00e1b5ea89
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-24 09:04:21 +0000

    container config done

--- ValidParanthesis.java
+++ ValidParanthesis.java
@@ -2,20 +2,17 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-
-        for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '{' || ch == '[') {
-                stack.push(ch);
-            } else {
-                if (stack.isEmpty()) return false;
-                 char top = stack.pop();
-                if ((ch == ')' && top != '(') ||
-                    (ch == '}' && top != '{') ||
-                    (ch == ']' && top != '[')) {
+        for (char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()) return false;
+                char top = stack.pop();
+                if( (c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top !='[')){
                     return false;
                 }
             }
         }
         return stack.isEmpty();
     }
-}
+}                                                      

dd78a5c19c26f49c644f465d8f01bd0f83bafa7d
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-24 15:28:05 +0000

    container config done

--- FractionToRecurringDecimal.java
+++ FractionToRecurringDecimal.java
@@ -4,3 +4,16 @@ class Solution {
         if (numerator == 0) return "0";
 
         StringBuilder sb = new StringBuilder();
+        //handle sign
+        if ((numerator < 0) ^ (denominator < 0 )) sb.append("-");
+
+        long num = Math.abs((long) numerator);
+        long den = Math.abs((long) denominator);
+
+        // integer part
+        sb.append(num/den);
+        long remainder = num % den;
+        if(remainder == 0) return sb.toString();
+
+        sb.append(".");
+        Map<Long, Integer> map = new HashMap<>();

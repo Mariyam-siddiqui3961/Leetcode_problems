4fee5a3046eb1b1a71181fe8017d0c8da08755f6
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-25 05:28:59 +0000

    container config done

--- Triangle.java
+++ Triangle.java
@@ -1,1 +1,7 @@
 //-------------------Traingle-------------------
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n];
+        for(int i = 0; i<n; i++){
+            dp[i] = triangle.get(n-1).get(i);

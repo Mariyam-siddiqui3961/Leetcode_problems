57995cb5d78e4a40a241e938178c279345ebdf95
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-25 05:49:54 +0000

    container config done

--- SumOfAbsoluteDifferenceInSortedArray.java
+++ SumOfAbsoluteDifferenceInSortedArray.java
@@ -1,1 +1,19 @@
 //---------Sum of absolite differences in sorted array---------
+class Solution {
+    public int[] getSumAbsoluteDifferences(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int[]prefix = new int [ n+1];
+
+        for(int i = 0; i<n; i++){
+            prefix[i+1] = prefix[i] + nums[i];
+        }
+        int total = prefix[n];
+        for (int i = 0; i<n; i++){
+            int left = nums[i]* i-prefix[i];
+            int right = (total - prefix[i+1] - nums[i]*(n-i-1));
+            result[i] = left + right;
+        }
+        return result;
+    }
+}

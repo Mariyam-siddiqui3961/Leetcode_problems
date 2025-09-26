26f28bc0909ccb67abff48ed78bcdf701a75b1be
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-26 16:28:07 +0000

    container config done

--- LinkedListCycle.java
+++ LinkedListCycle.java
@@ -1,1 +1,7 @@
 //---------------Linked List cycle----------
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head;

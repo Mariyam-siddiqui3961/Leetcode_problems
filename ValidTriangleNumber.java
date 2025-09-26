8ffe0898de74330839a5bd8608eef38b9eb23d7f
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-26 16:13:26 +0000

    container config done

--- ValidTriangleNumber.java
+++ ValidTriangleNumber.java
@@ -1,1 +1,7 @@
 //--------------------Valid Triangle Number-----------------
+
+class Solution {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int count = 0;

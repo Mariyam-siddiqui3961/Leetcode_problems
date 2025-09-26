6bbfb3cee64e10bfa5bf37a86403a9fa54c741c7
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-26 16:37:57 +0000

    container config done

---
+++ SwapNodeInPairs.java
@@ -0,0 +1,21 @@
+//-----------------swap node in pairs--------------------
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        while(prev.next != null && prev.next.next != null){
+            ListNode first = prev.next;
+            ListNode second = prev.next.next;
+
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+
+            //move prev to next pair
+            prev = first;
+        }
+        return dummy.next;
+    }
+}

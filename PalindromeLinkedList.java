608e344d096ff69b3598b934c3a68fea8fa06753
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-27 14:56:49 +0000

    container config done

--- PalindromeLinkedList.java
+++ PalindromeLinkedList.java
@@ -11,3 +11,27 @@ class Solution {
             slow = slow.next;
             fast = fast.next.next;
         }
+        //reverse second half
+        ListNode secondHalf = reverseList(slow);
+        ListNode copySecondHalf = secondHalf;    // to restore later if needed
+
+        // compare halves
+        ListNode firstHalf = head;
+        while(secondHalf != null){
+            if(firstHalf.val != secondHalf.val) return false;
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+        return true;  
+    }
+    private ListNode reverseList(ListNode head){
+        ListNode prev = null, curr = head;
+        while (curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}

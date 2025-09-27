08a98ed3dc0b2ac151179d44690b23d66946a0fd
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-27 14:56:17 +0000

    container config done

--- PalindromeLinkedList.java
+++ PalindromeLinkedList.java
@@ -1,3 +1,13 @@
 //-----------------Palindrome Linked List-------------
 class Solution {
     public boolean isPalindrome(ListNode head) {
+         if (head == null || head.next == null) return true;
+
+        //find middle
+        ListNode slow = head;       
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }

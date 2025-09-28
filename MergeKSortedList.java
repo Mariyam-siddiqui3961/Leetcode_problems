1639ed8c0cb3a3d3d018c551e8ebe3b8cf7f0580
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-28 11:19:01 +0000

    container config done

--- MergeKSortedList.java
+++ MergeKSortedList.java
@@ -1,1 +1,18 @@
 //---------------Merge k sorted list--------------------
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        return mergeLists(lists, 0, lists.length - 1);
+    }
+
+    private ListNode mergeLists(ListNode[] lists, int left, int right) {
+        if (left == right) return lists[left];
+        int mid = left + (right - left) / 2;
+        ListNode l1 = mergeLists(lists, left, mid);
+        ListNode l2 = mergeLists(lists, mid + 1, right);
+        return mergeTwoLists(l1, l2);
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;

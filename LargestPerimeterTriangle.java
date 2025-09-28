ae4083b94e6ef1ebec31d6f42a80ea1c0eddab02
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-28 11:32:50 +0000

    container config done

--- LargestPerimeterTriangle.java
+++ LargestPerimeterTriangle.java
@@ -1,1 +1,13 @@
 //------------Largest Perimeter Triangle ---------------
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);  // sort ascending
+        // check triplets from the largest side
+        for (int i = nums.length - 1; i >= 2; i--) {
+            if (nums[i - 2] + nums[i - 1] > nums[i]) {
+                return nums[i] + nums[i - 1] + nums[i - 2];
+            }
+        }
+        return 0;
+    }
+}

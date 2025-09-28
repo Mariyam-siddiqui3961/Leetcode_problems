43527105d31a4e73c0362db82adc14e31ce34aca
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-28 11:24:01 +0000

    container config done

--- ComputeDecimalRepresentation.java
+++ ComputeDecimalRepresentation.java
@@ -1,1 +1,24 @@
 //---------------Decimal Representation -------------------------
+class Solution {
+    public int[] decimalRepresentation(int n) {
+        List<Integer> components = new ArrayList<>();
+        int place = 1;
+
+        while(n>0){
+            int digit  = n % 10;
+            if(digit != 0){
+                components.add(digit * place);
+            }
+            n /= 10;
+            place *= 10;
+        }
+
+        components.sort(Collections.reverseOrder());
+
+        int[] result = new int[components.size()];
+        for (int i = 0; i<components.size(); i++){
+            result[i] = components.get(i);
+        }
+        return result;
+    }
+}

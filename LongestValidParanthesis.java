722b22fa97a26e6f97ddcd105047029dac860923
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-29 15:51:34 +0000

    container config done

--- LongestValidParanthesis.java
+++ LongestValidParanthesis.java
@@ -1,1 +1,9 @@
 //---------------Longest Valid Parenthesis----------------
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int maxLen = 0;
+        for(int i = 0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '('){

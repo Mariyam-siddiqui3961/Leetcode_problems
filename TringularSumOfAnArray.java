807ee986aa70b5f3a6539b4112eb9e8115dba85f
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-30 04:10:04 +0000

    container config done

--- TringularSumOfAnArray.java
+++ TringularSumOfAnArray.java
@@ -1,1 +1,12 @@
 //---------------Triangular Sum of An Array--------------
+class Solution {
+    public int triangularSum(int[] nums) {
+        int n = nums.length;
+        for(int size = n; size>1; size--){
+            for(int i = 0; i<size-1; i++){
+                nums[i] = (nums[i] + nums[i+1])%10;
+            }
+        }
+        return nums[0];
+    }
+}

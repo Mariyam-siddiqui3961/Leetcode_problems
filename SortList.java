ef8c54348ba9692e6ea68b382a06b94d65ea8a56
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-30 04:04:46 +0000

    container config done

--- SortList.java
+++ SortList.java
@@ -1,1 +1,30 @@
 //------------------Sort List--------------------
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while(fast != null && fast.next !=null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+
+        return merge(l1, l2);
+    }
+    private ListNode merge(ListNode l1, ListNode l2){
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while(l1 != null && l2 != null){
+            if(l1.val< l2.val){
+                curr.next = l1;
+                l1 = l1.next;
+            }else{

a7f2c00ae48bdeaaa494dd38e39332256e178976
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-01 16:31:46 +0000

    container config done

--- WaterBottle.java
+++ WaterBottle.java
@@ -1,1 +1,5 @@
 //----------------------Water Bottle-----------------------
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int totalDrank = numBottles;  // Initially drink all full bottles
+        int empty = numBottles;       // All are now empty

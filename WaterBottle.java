3cc15dc1a2afb9e328de3bbc962b3ebecbd3ce30
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-01 16:32:17 +0000

    container config done

--- WaterBottle.java
+++ WaterBottle.java
@@ -3,3 +3,12 @@ class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
         int totalDrank = numBottles;  // Initially drink all full bottles
         int empty = numBottles;       // All are now empty
+        while (empty >= numExchange) {
+            int newFull = empty / numExchange;   // how many new full bottles we get
+            totalDrank += newFull;              // drink them
+            empty = empty % numExchange + newFull; // leftover empty + new empty bottles
+        }
+
+        return totalDrank;
+    }
+}

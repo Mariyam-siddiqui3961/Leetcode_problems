87a87f22a946dd357954006444249c46045bac38
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-02 15:57:03 +0000

    container config done

--- WaterBottlesII.java
+++ WaterBottlesII.java
@@ -1,1 +1,5 @@
 //---------------------Water Bottles II----------------------
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int totalDrank = numBottles;   // Drink all initial full bottles
+        int emptyBottles = numBottles; // All become empty after drinking

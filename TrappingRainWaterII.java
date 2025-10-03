dcab9baebfadf10a39d091a64d4080cbe1d6408b
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-03 05:45:39 +0000

    container config done

--- TrappingRainWaterII.java
+++ TrappingRainWaterII.java
@@ -4,3 +4,13 @@ class Solution {
         int m = heightMap.length, n = heightMap[0].length;
         //no space to hold water
         if(m<=2 || n<=2) return 0;
+        boolean[][] visited = new boolean[m][n];
+         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[2]));
+
+        //push all boundry cells
+        for (int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(i == 0 || j == 0|| i == m-1 || j == n-1){
+                    pq.offer(new int[]{i,j,heightMap[i][j]});
+                    visited[i][j] = true;
+                }

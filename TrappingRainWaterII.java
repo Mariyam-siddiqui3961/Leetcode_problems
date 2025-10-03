a0b2b36f1f79fdd47a9e6572f9ad68dfd1d21323
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-03 05:45:05 +0000

    container config done

--- TrappingRainWaterII.java
+++ TrappingRainWaterII.java
@@ -1,1 +1,6 @@
 //--------------------Trapping rain water --------------------
+class Solution {
+    public int trapRainWater(int[][] heightMap) {
+        int m = heightMap.length, n = heightMap[0].length;
+        //no space to hold water
+        if(m<=2 || n<=2) return 0;

1e6942541aa40c12c12ebf1b85a9f178f86dfa48
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-04 04:31:47 +0000

    container config done

--- MinStack.java
+++ MinStack.java
@@ -1,1 +1,14 @@
 //----------------------MinStack----------------------
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+    
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(minStack.isEmpty() || val <= minStack.peek()){
+            minStack.push(val);

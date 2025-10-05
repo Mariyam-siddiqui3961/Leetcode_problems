b0be75cf20944417d5cd3cea65a6e0bdcd3375d6
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-05 16:52:54 +0000

    container config done

--- ComputerAlternatingSum.java
+++ ComputerAlternatingSum.java
@@ -1,1 +1,8 @@
 //----------------------Computer Alternating Sum-------------------
+class Solution {
+    public int alternatingSum(int[] nums) {
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0)
+                sum += nums[i];   // add if index is even

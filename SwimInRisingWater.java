e209af35f848e5c953393f888ae9e47a30a474d7
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-06 05:59:08 +0000

    container config done

--- SwimInRisingWater.java
+++ SwimInRisingWater.java
@@ -1,1 +1,12 @@
 //---------------------Swim in rising water---------------------------
+class Solution {
+    public int swimInWater(int[][] grid) {
+         int n = grid.length;
+        int[][] directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        boolean[][] visited = new boolean[n][n];
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        pq.offer(new int[]{grid[0][0], 0, 0}); // {height, row, col}
+        visited[0][0] = true;
+        
+        int time = 0;

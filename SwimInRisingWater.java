5574aeabe09a42d6f888b1fc035dd684ad4316d9
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-06 05:59:37 +0000

    container config done

--- SwimInRisingWater.java
+++ SwimInRisingWater.java
@@ -10,3 +10,21 @@ class Solution {
         visited[0][0] = true;
         
         int time = 0;
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int height = curr[0], r = curr[1], c = curr[2];
+            
+            time = Math.max(time, height);
+            if (r == n - 1 && c == n - 1) return time;
+            
+            for (int[] dir : directions) {
+                int nr = r + dir[0], nc = c + dir[1];
+                if (nr < 0 || nc < 0 || nr >= n || nc >= n || visited[nr][nc]) continue;
+                
+                visited[nr][nc] = true;
+                pq.offer(new int[]{grid[nr][nc], nr, nc});
+            }
+        }
+        return -1;
+    }
+}

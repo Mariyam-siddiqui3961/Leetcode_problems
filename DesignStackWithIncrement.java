064624813571cb6749977a2344a466e796de5832
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-06 11:59:08 +0000

    container config done

--- DesignStackWithIncrement.java
+++ DesignStackWithIncrement.java
@@ -1,1 +1,13 @@
 //--------------------Design Stack with Inceremnt-----------------------
+class CustomStack {
+    private int[] stack;
+    private int[] inc;
+    private int top;
+    private int maxSize;
+    
+    public CustomStack(int maxSize) {
+        this.stack = new int[maxSize];
+        this.inc = new int[maxSize];
+        this.top = -1;
+        this.maxSize = maxSize;
+    }

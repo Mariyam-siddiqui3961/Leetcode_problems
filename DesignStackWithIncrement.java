c96cba246b75d534f63af1bc48de5075a475c965
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-06 11:59:42 +0000

    container config done

--- DesignStackWithIncrement.java
+++ DesignStackWithIncrement.java
@@ -11,3 +11,17 @@ class CustomStack {
         this.top = -1;
         this.maxSize = maxSize;
     }
+    public void push(int x) {
+        if (top + 1 < maxSize) {
+            stack[++top] = x;
+        }
+    }
+    
+    public int pop() {
+        if (top == -1) return -1;
+        int res = stack[top] + inc[top];
+        
+        // propagate increment to below elements
+        if (top > 0) {
+            inc[top - 1] += inc[top];
+        }

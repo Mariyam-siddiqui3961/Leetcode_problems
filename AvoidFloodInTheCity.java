70b1dc61e036d8695845dc7bb3a4adcf158dc348
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-07 05:36:42 +0000

    container config done

--- AvoidFloodInTheCity.java
+++ AvoidFloodInTheCity.java
@@ -1,1 +1,7 @@
 //----------------Avoid Flood in the City-----------------------
+class Solution {
+    public int[] avoidFlood(int[] rains) {
+        int n = rains.length;
+        int[] ans = new int[n];
+        Map<Integer, Integer> fullLakes = new HashMap<>();
+        TreeSet<Integer> dryDays = new TreeSet<>();

4592c09b0b73ec9d2d6fc5bc28ffecfb52a384f7
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-07 14:52:27 +0000

    container config done

--- MedianOfTwoSortedArrays.java
+++ MedianOfTwoSortedArrays.java
@@ -1,1 +1,7 @@
 //-----------------Median of Two Sorted Arrays-------------
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        int total = m + n;
+        int mid1 = (total - 1) / 2;
+        int mid2 = total / 2;

3f8bede33165b3bad0f5005b9106958186d73b0a
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-07 05:37:39 +0000

    container config done

--- AvoidFloodInTheCity.java
+++ AvoidFloodInTheCity.java
@@ -13,3 +13,15 @@ class Solution {
                 ans[i] = 1; // default, might change later
             } else {
                 ans[i] = -1; // raining day
+                if (fullLakes.containsKey(lake)) {
+                    // we need to find a dry day AFTER last time it was filled
+                    int lastRainDay = fullLakes.get(lake);
+                    Integer dryDay = dryDays.higher(lastRainDay);
+                    if (dryDay == null) {
+                        // no available dry day to prevent flood
+                        return new int[0];
+                    }
+                    // assign this dry day to dry this lake
+                    ans[dryDay] = lake;
+                    dryDays.remove(dryDay);
+                }

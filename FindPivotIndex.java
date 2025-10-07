dcdef56328f4dfed5a9068265c84fc8bf9c0a0cf
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-07 16:26:08 +0000

    container config done

--- FindPivotIndex.java
+++ FindPivotIndex.java
@@ -1,1 +1,20 @@
 //--------------------find pivot index---------------------
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int total = 0;
+        for (int num : nums) {
+            total += num;
+        }
+
+        int leftTotal = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int rightTotal = total - leftTotal - nums[i];
+            if (rightTotal == leftTotal){
+                return i;
+            }
+            leftTotal += nums[i];
+        }
+
+        return -1;
+    }
+}

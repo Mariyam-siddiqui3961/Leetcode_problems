27657f2504a2195824a9a343bb0c44cc70115fd5
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-08 13:26:31 +0000

    container config done

---
+++ MaximunProductOfThreeNumbers.java
@@ -0,0 +1,5 @@
+//-------------------Maximun Product of three numbers-----------------
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;

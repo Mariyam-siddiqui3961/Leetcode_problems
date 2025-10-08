494792d7846e873e85be45da4c5c5180f0f93bd9
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-08 13:29:48 +0000

    container config done

--- MaxConsecutiveOnes.java
+++ MaxConsecutiveOnes.java
@@ -5,3 +5,13 @@ class Solution {
         int max = 0;
 
         for(int i=0; i<nums.length; i++){
+            if(nums[i] == 1){
+                count++;
+                max = Math.max(max, count);
+            }else{
+                count = 0;
+            }
+        }
+        return max;
+    }
+}

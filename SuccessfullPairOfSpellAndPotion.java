8d5acc5dc413c22798c21eaf0a17cc606e0e1813
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-09 14:00:36 +0000

    container config done

--- SuccessfullPairOfSpellAndPotion.java
+++ SuccessfullPairOfSpellAndPotion.java
@@ -1,1 +1,21 @@
 //------------------successfull pair of spell and potions ----------------------
+import java.util.*;
+
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort(potions);
+        int n = spells.length;
+        int m = potions.length;
+        int[] answer = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            long spell = spells[i];
+            // Minimum potion strength needed
+            long required = (success + spell - 1) / spell; // ceiling division
+
+            // Binary search for first potion >= required
+            int idx = lowerBound(potions, required);
+            
+            answer[i] = m - idx; // remaining potions are successful
+        }
+        return answer;

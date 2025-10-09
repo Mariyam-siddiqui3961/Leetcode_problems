d6b29231042a8babbc9d687ffd79ab3f791d6828
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-09 14:03:48 +0000

    container config done

--- MaximumAmountOfTimeToBrewPotion.java
+++ MaximumAmountOfTimeToBrewPotion.java
@@ -14,3 +14,22 @@ class Solution {
         for (int j = 1; j < m; ++j) {
             // prevPotionDone represents finish time of wizard (n-1) for potion j-1
             long prevPotionDone = prevWizardDone;
+            // iterate wizards from n-2 down to 0 (we update prevWizardDone greedily for new potion j)
+            for (int i = n - 2; i >= 0; --i) {
+                // remove time of wizard i+1 for previous potion (we shift reference window)
+                prevPotionDone -= (long) skill[i + 1] * (long) mana[j - 1];
+
+                // The earliest finish time for wizard i (for potion j) cannot be earlier than:
+                //  - prevPotionDone (the finishing time carried from the later wizard pipeline)
+                //  - prevWizardDone - skill[i]*mana[j] (what was last wizard's finish reduced by new wizard's processing)
+                prevWizardDone = Math.max(prevPotionDone,
+                                          prevWizardDone - (long) skill[i] * (long) mana[j]);
+            }
+
+            // after computing adjustments, add the total time for all wizards to process potion j
+            prevWizardDone += sumSkill * (long) mana[j];
+        }
+
+        return prevWizardDone;
+    }
+}

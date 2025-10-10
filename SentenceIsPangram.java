2bc23a69c45a47e168af497d16f72ef8c797cb89
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-10 15:48:43 +0000

    container config done

--- SentenceIsPangram.java
+++ SentenceIsPangram.java
@@ -1,1 +1,6 @@
 //--------------check if sentence is pangram------------------------
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        if(sentence.length() < 26) return false;
+
+        boolean[] seen = new boolean[26];

45d01e2f523ccb852fdaacbc9f3504bf6dd4168d
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-10 15:38:13 +0000

    container config done

--- TakingMaximumEnergyFromMysticDungeon.java
+++ TakingMaximumEnergyFromMysticDungeon.java
@@ -1,1 +1,8 @@
 //--------------Taking maximum energy from mystic dungeon---------------
+class Solution {
+    public int maximumEnergy(int[] energy, int k) {
+        int n = energy.length;
+        int[] dp = new int[n];
+        int maxEnergy = Integer.MIN_VALUE;
+        for(int i = n-1; i>=0; i--){
+            if(i+k<n){

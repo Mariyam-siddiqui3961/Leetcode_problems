b874cf408e10635ebc4fdf330891467ad85c770d
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-11 05:35:57 +0000

    container config done

--- MaximumTotalDamage.java
+++ MaximumTotalDamage.java
@@ -6,3 +6,15 @@ class Solution {
         for (int p : power) {
             sumMap.put(p, sumMap.getOrDefault(p, 0L) + (long)p);
         }
+        // Sort unique damage values
+        int n = sumMap.size();
+        int[] keys = new int[n];
+        int idx = 0;
+        for (int k : sumMap.keySet()) keys[idx++] = k;
+        Arrays.sort(keys);
+
+        long[] sums = new long[n];
+        for (int i = 0; i < n; i++) sums[i] = sumMap.get(keys[i]);
+
+        long[] dp = new long[n];
+        for (int i = 0; i < n; i++) {

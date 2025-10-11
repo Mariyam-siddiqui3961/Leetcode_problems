363ab5d471f30ea4a84b10814e8492076140ba80
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-11 16:15:18 +0000

    container config done

--- ReverseLinkedListII.java
+++ ReverseLinkedListII.java
@@ -1,1 +1,12 @@
 //--------------reverse linked list II-------------------
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        // Step 1: Create a dummy node to simplify edge cases
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        // Step 2: Move `prev` to the node before 'left'
+        ListNode prev = dummy;
+        for (int i = 1; i < left; i++) {
+            prev = prev.next;
+        }

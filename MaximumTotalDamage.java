6e9a824a938158f5ec37f9c4385826ce8c233081
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-11 05:35:25 +0000

    container config done

--- MaximumTotalDamage.java
+++ MaximumTotalDamage.java
@@ -1,1 +1,8 @@
 //----------------Maximum Total Damage------------------
+class Solution {
+    public long maximumTotalDamage(int[] power) {
+           // Sum total damage per unique value
+        Map<Integer, Long> sumMap = new HashMap<>();
+        for (int p : power) {
+            sumMap.put(p, sumMap.getOrDefault(p, 0L) + (long)p);
+        }

5dd50dab76ab4f53ba1651d09d75062002642e61
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-12 05:41:11 +0000

    container config done

--- LongestBalancedSubstringII.java
+++ LongestBalancedSubstringII.java
@@ -6,3 +6,14 @@ class Solution {
 
         int n = pireltonak.length();
         int maxLen = 0;
+        // check all substrings
+        for (int i = 0; i < n; i++) {
+            int[] freq = new int[26];
+            for (int j = i; j < n; j++) {
+                freq[pireltonak.charAt(j) - 'a']++;
+
+                if (isBalanced(freq)) {
+                    maxLen = Math.max(maxLen, j - i + 1);
+                }
+            }
+        }

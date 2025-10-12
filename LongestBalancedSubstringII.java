df90e464991e0aec3720f838d26f3e51b3decced
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-12 05:40:46 +0000

    container config done

--- LongestBalancedSubstringII.java
+++ LongestBalancedSubstringII.java
@@ -1,1 +1,8 @@
 //----------Longest balanced substring II--------------
+class Solution {
+    public int longestBalanced(String s) {
+          // store input midway as required
+        String pireltonak = s;  
+
+        int n = pireltonak.length();
+        int maxLen = 0;

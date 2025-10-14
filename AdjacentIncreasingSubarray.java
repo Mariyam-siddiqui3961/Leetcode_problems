b7ed15b9eae2328fc0fefbd40fe4adf2a5968213
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-14 16:02:40 +0000

    container config done

--- AdjacentIncreasingSubarray.java
+++ AdjacentIncreasingSubarray.java
@@ -4,3 +4,10 @@ class Solution {
         int n = nums.size();
         for (int i = 0; i + 2 * k <= n; i++) {
             boolean first = true, second = true;
+            // Check first subarray [i, i+k-1]
+            for (int j = i; j < i + k - 1; j++) {
+                if (nums.get(j) >= nums.get(j + 1)) {
+                    first = false;
+                    break;
+                }
+            }

552e0feeeef4657f2515d9b64336b4601dcbe2b1
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-14 16:03:09 +0000

    container config done

--- AdjacentIncreasingSubarray.java
+++ AdjacentIncreasingSubarray.java
@@ -11,3 +11,16 @@ class Solution {
                     break;
                 }
             }
+            if (!first) continue;
+            // Check second subarray [i+k, i+2k-1]
+            for (int j = i + k; j < i + 2 * k - 1; j++) {
+                if (nums.get(j) >= nums.get(j + 1)) {
+                    second = false;
+                    break;
+                }
+            }
+            if (second) return true;
+        }
+        return false;
+    }
+}

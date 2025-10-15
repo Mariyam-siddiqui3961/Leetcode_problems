93c58b6902947478a62f072767a0724b24034802
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-15 17:51:18 +0000

    container config done

--- AdjacentIncreasingSubarrayII.java
+++ AdjacentIncreasingSubarrayII.java
@@ -1,1 +1,6 @@
 //-----------Adjacent Increasing Subarray II--------------
+class Solution {
+    public int maxIncreasingSubarrays(List<Integer> nums) {
+        int n=nums.size();
+        int ans = 0;
+        int prev = 0, curr=1;

159b0e57b5877073f7f5c660cc3af44bc1aad169
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-16 15:15:19 +0000

    container config done

--- SmallestMissingNonNegativeInteger.java
+++ SmallestMissingNonNegativeInteger.java
@@ -1,1 +1,5 @@
 //--------------Smallest missing non negative integer--------------
+class Solution {
+    public int findSmallestInteger(int[] nums, int value) {
+        int n = nums.length;
+        Map<Integer, Integer> remainderCount = new HashMap<>();

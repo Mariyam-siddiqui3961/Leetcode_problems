2a30d40a7e7c1c692fecd2d01b0d43cac9193778
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-17 09:22:23 +0000

    container config done

--- MaximizeNumberOfPartitionAfterOperation.java
+++ MaximizeNumberOfPartitionAfterOperation.java
@@ -1,1 +1,8 @@
 //---------Maximize number of Partition after operation--------------------
+class Solution {
+    public int maxPartitionsAfterOperations(String s, int k) {
+        Map<Long, Integer> mem = new HashMap<>();
+        // dp(0, true, 0) returns number of extra partitions from index 0,
+        // +1 accounts for the first partition counted at the end.
+        return dp(s, 0, true, 0, k, mem) + 1;
+    }

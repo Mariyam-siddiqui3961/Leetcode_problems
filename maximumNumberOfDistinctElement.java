68f0d1c8201c1290936644df435315bb3f74b0f2
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-18 11:26:38 +0000

    container config done

--- maximumNumberOfDistinctElement.java
+++ maximumNumberOfDistinctElement.java
@@ -1,1 +1,5 @@
 //-------------maximum number of distinct element----------------
+class Solution {
+    public int maxDistinctElements(int[] nums, int k) {
+        Arrays.sort(nums);
+        long last = Long.MIN_VALUE;

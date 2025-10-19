6e7c1a89ddaf041ecf4eec51cd91f0711b162dd0
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-19 08:04:34 +0000

    container config done

--- MinimumToRemoveToMakeValidParenthesis.java
+++ MinimumToRemoveToMakeValidParenthesis.java
@@ -1,1 +1,5 @@
 //--------------minimum to remove to make valid parenthesis------------------
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder();
+        int open = 0;

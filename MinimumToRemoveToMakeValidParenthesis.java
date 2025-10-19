4ac2032990055919d8b2c03266ca6d4cc59ae197
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-19 08:05:14 +0000

    container config done

--- MinimumToRemoveToMakeValidParenthesis.java
+++ MinimumToRemoveToMakeValidParenthesis.java
@@ -3,3 +3,11 @@ class Solution {
     public String minRemoveToMakeValid(String s) {
         StringBuilder sb = new StringBuilder();
         int open = 0;
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                open++;
+                sb.append(c);
+            }else if (c == ')'){
+                if(open > 0){
+                    open--;
+                    sb.append(c);

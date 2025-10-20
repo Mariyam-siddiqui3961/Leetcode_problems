057fc9af6aa3d5bb1c1f7c6d49643d5553a953be
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-20 12:51:43 +0000

    container config done

--- FinalValueOfVariableAfterOperation.java
+++ FinalValueOfVariableAfterOperation.java
@@ -2,3 +2,6 @@
 class Solution {
     public int finalValueAfterOperations(String[] operations) {
         int x = 0;
+        for(String op : operations){
+            if(op.contains("++")){
+                x++;

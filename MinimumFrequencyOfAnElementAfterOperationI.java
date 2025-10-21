c728210f1cae029f6e1142ceb50e7f8db095af40
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-21 08:23:29 +0000

    container config done

--- MinimumFrequencyOfAnElementAfterOperationI.java
+++ MinimumFrequencyOfAnElementAfterOperationI.java
@@ -1,1 +1,5 @@
 //------------minimum fre of an element after operation ----------------
+public class Solution {
+    public int maxFrequency(int[] nums, int k, int numOperations) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        TreeMap<Integer, Integer> diff = new TreeMap<>();

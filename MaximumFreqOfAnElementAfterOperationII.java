870213ec83754150f88afde74dc235b8e4120bc7
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-22 07:45:47 +0000

    container config done

--- MaximumFreqOfAnElementAfterOperationII.java
+++ MaximumFreqOfAnElementAfterOperationII.java
@@ -3,3 +3,9 @@ class Solution {
     public int maxFrequency(int[] nums, int k, int numOperations) {
         Map<Long, Integer> count = new HashMap<>();
         Map<Long, Integer> diff = new HashMap<>();
+         for(int num : nums){
+            long x = (long) num;
+            count.put(x, count.getOrDefault(x,0) +1);
+            diff.put(x-k, diff.getOrDefault(x-k, 0) +1);
+            diff.put(x+k+1, diff.getOrDefault(x+k+1, 0) -1);
+        }

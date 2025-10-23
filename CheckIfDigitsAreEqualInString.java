51f941db42555f1e55669e39bfa5e2807e1566c6
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-23 07:29:31 +0000

    container config done

--- CheckIfDigitsAreEqualInString.java
+++ CheckIfDigitsAreEqualInString.java
@@ -4,3 +4,9 @@ class Solution {
         final int n = s.length();
         int num1 = 0, num2 = 0;
         for (int i = 0; i + 1 < n; ++i) {
+             int coefficient = nCkMod10(n - 2, i);
+            num1 = (num1 + coefficient * (s.charAt(i) - '0')) % 10;
+            num2 = (num2 + coefficient * (s.charAt(i + 1) - '0')) % 10;
+        }
+        return num1 == num2;
+    }

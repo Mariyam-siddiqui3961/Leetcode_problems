8c285f5f49b9332a15e7079d0573f7e0a4dc0768
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-23 07:28:41 +0000

    container config done

--- CheckIfDigitsAreEqualInString.java
+++ CheckIfDigitsAreEqualInString.java
@@ -1,1 +1,6 @@
 //-----------check if digits are equal in string-------------
+class Solution {
+    public boolean hasSameDigits(String s) {
+        final int n = s.length();
+        int num1 = 0, num2 = 0;
+        for (int i = 0; i + 1 < n; ++i) {

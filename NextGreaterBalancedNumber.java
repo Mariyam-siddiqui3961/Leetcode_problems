9d6e6dd2c82dedf8b639f608c02d18b22629a0d0
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-24 13:33:14 +0000

    container config done

--- NextGreaterBalancedNumber.java
+++ NextGreaterBalancedNumber.java
@@ -1,1 +1,8 @@
 //------------Next Greater Balanced Number-----------
+class Solution {
+    public int nextBeautifulNumber(int n) {
+        for(int num =n+1; num<= 1224444; num++){
+            if(isBalanced(num)) return num;
+        }
+        return -1;
+    }

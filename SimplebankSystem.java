0f026ced616cae66bd9ca4313c8ae6e3d6d352ef
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-26 11:53:58 +0000

    container cofig done

--- SimplebankSystem.java
+++ SimplebankSystem.java
@@ -6,3 +6,14 @@ class Bank {
         this.balance = balance;
         
     }
+     public boolean transfer(int account1, int account2, long money) {
+        if (!isValidAccount(account1) || !isValidAccount(account2))
+        return false;
+        if(balance[account1 -1] < money)
+        return false;
+
+        balance[account1 - 1] -= money;
+        balance[account2 -1] += money;
+        return true;
+        
+    }

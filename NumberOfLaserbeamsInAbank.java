db6c2ebebe65b8aaf0f6619bb105afb4a7847407
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-27 14:11:03 +0000

    container config done

--- NumberOfLaserbeamsInAbank.java
+++ NumberOfLaserbeamsInAbank.java
@@ -3,3 +3,7 @@ class Solution {
     public int numberOfBeams(String[] bank) {
         int prev = 0;
         int totalBeam = 0;
+         for (String row : bank) {
+            int curr = 0;
+            for( char c: row.toCharArray()){
+                if(c=='1') curr++;

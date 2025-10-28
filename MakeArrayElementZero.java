6f34ea03f46deb1cccd34216fe5d5d4b3d16d3f3
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-28 13:05:53 +0000

    container config done

--- MakeArrayElementZero.java
+++ MakeArrayElementZero.java
@@ -1,1 +1,7 @@
 //---------Make array element to zero-----------------
+class Solution {
+    public int countValidSelections(int[] nums) {
+        int n = nums.length;
+        int total = 0;
+        int totalSum = 0;
+        for(int v : nums) totalSum += v;

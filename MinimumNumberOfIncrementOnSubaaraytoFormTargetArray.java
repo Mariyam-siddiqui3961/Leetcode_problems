9f13d5ed86a2075daf1add77891548dffb591e4c
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-30 14:45:34 +0000

    container config done

--- MinimumNumberOfIncrementOnSubaaraytoFormTargetArray.java
+++ MinimumNumberOfIncrementOnSubaaraytoFormTargetArray.java
@@ -2,3 +2,5 @@
 class Solution {
     public int minNumberOperations(int[] target) {
         int operations = target[0];
+        for(int i = 1; i< target.length; i++){
+            if(target[i] >target[i-1]){

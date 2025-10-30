bf88585447e0f2a1ca39dd3a621343a5062d173e
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-30 14:46:01 +0000

    container config done

--- MinimumNumberOfIncrementOnSubaaraytoFormTargetArray.java
+++ MinimumNumberOfIncrementOnSubaaraytoFormTargetArray.java
@@ -4,3 +4,6 @@ class Solution {
         int operations = target[0];
         for(int i = 1; i< target.length; i++){
             if(target[i] >target[i-1]){
+                 operations += target[i] - target[i-1];
+            }
+        }

343af04f91871ab00420d07fa3c96036fc77d879
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-10-31 12:27:54 +0000

    container config done

--- ThetwoSneakyNumbersOfDigitVille.java
+++ ThetwoSneakyNumbersOfDigitVille.java
@@ -2,3 +2,6 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
         int n = nums.length;
+        int[] count = new int[n];
+        int[] result = new int[2];
+        int idx=0;

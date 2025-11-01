c251936d462b31ffa64472f8101fe0affe31d4e6
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-01 04:07:29 +0000

    container config done

--- DeleteNodesFromLinkedListPresentInArray.java
+++ DeleteNodesFromLinkedListPresentInArray.java
@@ -1,1 +1,7 @@
 //----------delete nodes from linked list present in array-------------
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num: nums){
+            set.add(num);
+        }

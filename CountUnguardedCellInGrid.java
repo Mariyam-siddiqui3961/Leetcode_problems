f17c32a9db579bd3fb9a6a595ffe0b6206de4d4d
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-02 05:20:38 +0000

    container config done

--- CountUnguardedCellInGrid.java
+++ CountUnguardedCellInGrid.java
@@ -1,1 +1,8 @@
 //-----count unguarded cell in grid-----------
+class Solution {
+    public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
+         // 0 = empty, 1 = guard, 2 = wall, 3 = guarded
+        int[][] grid = new int[m][n];
+
+        for (int[] g : guards) grid[g[0]][g[1]] = 1;
+        for (int[] w : walls) grid[w[0]][w[1]] = 2;

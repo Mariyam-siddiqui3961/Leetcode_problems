1b0c0591b1a9aa65aeb75afc7353e5d06b77a587
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-02 05:23:23 +0000

    container config done

--- CountUnguardedCellInGrid.java
+++ CountUnguardedCellInGrid.java
@@ -47,4 +47,16 @@ class Solution {
             }
         }
 
+        // ---- Count unguarded empty cells ----
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) count++;
+            }
+        }
+
+        return count;
+    }
+}
+
         

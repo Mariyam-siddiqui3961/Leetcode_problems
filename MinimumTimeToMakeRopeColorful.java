1ff678a95b3f293c876730115130bd2d488be65e
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-03 05:40:32 +0000

    container config done

--- MinimumTimeToMakeRopeColorful.java
+++ MinimumTimeToMakeRopeColorful.java
@@ -3,3 +3,8 @@ class Solution {
     public int minCost(String colors, int[] neededTime) {
         int totalTime = 0;
         int maxTime = neededTime[0];  // max in current group
+        for(int i = 1; i<colors.length(); i++){
+            if(colors.charAt(i) == colors.charAt(i-1)){
+                totalTime += Math.min(maxTime, neededTime[i]);
+                maxTime = Math.max(maxTime, neededTime[i]);
+            }else{

0b37533d60ec6b50e292d803754f2e3d6f0b3e73
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-04 15:37:03 +0000

    container config done

--- FindXSumOfAllKLongSubarrayI.java
+++ FindXSumOfAllKLongSubarrayI.java
@@ -1,1 +1,7 @@
 //----find x-sum of all k-long subarray I------------
+class Solution {
+    public int[] findXSum(int[] nums, int k, int x) {
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+
+        for (int start = 0; start <= n - k; start++) {

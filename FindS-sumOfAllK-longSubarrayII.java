ec26e37db0408008813ded8bce69021211b78136
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-05 06:56:15 +0000

    container config done

--- FindS-sumOfAllK-longSubarrayII.java
+++ FindS-sumOfAllK-longSubarrayII.java
@@ -1,1 +1,10 @@
 //---------find x-sum of all k-long subarrayII----------
+import java.util.*;
+import java.util.function.Consumer;
+
+class Solution {
+
+    static class Node implements Comparable<Node> {
+        int val;
+        int freq;
+        Node(int v, int f) { val = v; freq = f; }

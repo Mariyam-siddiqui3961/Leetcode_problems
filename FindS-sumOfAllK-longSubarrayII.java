4ebecb95794e4897c8d14e3c86eb19562971d8d4
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-05 06:57:33 +0000

    container config done

--- FindS-sumOfAllK-longSubarrayII.java
+++ FindS-sumOfAllK-longSubarrayII.java
@@ -36,3 +36,21 @@ class Solution {
         Map<Integer, Integer> freq = new HashMap<>();
         TreeSet<Node> top = new TreeSet<>();   // holds up to x best nodes
         TreeSet<Node> rest = new TreeSet<>();  // holds the others
+
+        long[] sumTop = new long[1]; // mutable inside lambdas
+
+        // balance ensures top.size() <= x and top contains the best x nodes
+        Runnable balance = () -> {
+            // promote from rest until top has x items (or rest empty)
+            while (top.size() < x && !rest.isEmpty()) {
+                Node best = rest.pollFirst(); // best in rest
+                top.add(best);
+                sumTop[0] += 1L * best.val * best.freq;
+            }
+
+            // if top has more than x, demote worst
+            while (top.size() > x) {
+                Node worst = top.pollLast();
+                rest.add(worst);
+                sumTop[0] -= 1L * worst.val * worst.freq;
+            }

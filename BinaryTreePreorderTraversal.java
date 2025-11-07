5fbf017a6f9603aed865610dfe4f65b6ef0c9d36
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-07 16:09:25 +0000

    container config done

--- BinaryTreePreorderTraversal.java
+++ BinaryTreePreorderTraversal.java
@@ -1,1 +1,5 @@
 //--------------Binary tree preorder traversal--------------
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preorder(root, result);

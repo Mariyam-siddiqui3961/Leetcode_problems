fbb55e024414bf1ff4ca91bfd35fe7915891b1a4
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-07 16:04:26 +0000

    container config done

--- BinarytreeInorderTraversal.java
+++ BinarytreeInorderTraversal.java
@@ -1,1 +1,6 @@
 //-------binary tree inorder traversal-------------
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+        return result;

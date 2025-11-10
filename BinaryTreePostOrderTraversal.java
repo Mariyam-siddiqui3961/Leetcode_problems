8e36c3ed9c998937c63a41d98f049e301ace8fa9
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-10 16:09:12 +0000

    container config done

--- BinaryTreePostOrderTraversal.java
+++ BinaryTreePostOrderTraversal.java
@@ -2,3 +2,8 @@
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
+         postorder(root,result);
+        return result;
+    }
+
+    private void postorder(TreeNode node, List<Integer> result){

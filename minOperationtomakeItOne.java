78468fa5940735556de6b96e4999cbaa3a743771
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-12 17:01:17 +0000

    container config done

--- minOperationtomakeItOne.java
+++ minOperationtomakeItOne.java
@@ -6,3 +6,11 @@ class Solution {
         //count 1
         int count1 =0;
         for(int x : nums) if (x == 1) count1++;
+         // if already have one we only need(n - count 1)
+        if(count1 > 0) return n- count1;
+
+        //find shortest subarray with gcd = 1
+        int minLen = Integer.MAX_VALUE;
+
+        for(int i = 0; i<n; i++){
+            int gcd = nums[i];

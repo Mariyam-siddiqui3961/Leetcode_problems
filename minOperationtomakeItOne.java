975a75ae68ecffee6fe665352b5464eac611f60f
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-12 17:00:45 +0000

    container config done

--- minOperationtomakeItOne.java
+++ minOperationtomakeItOne.java
@@ -1,1 +1,8 @@
 //-----------------Minimum nmber of operation to amke element 1-----------------
+class Solution {
+    public int minOperations(int[] nums) {
+        int n = nums.length;
+
+        //count 1
+        int count1 =0;
+        for(int x : nums) if (x == 1) count1++;

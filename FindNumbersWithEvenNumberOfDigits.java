d188d197b4f22e97af31ada81872dec110678da8
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-13 15:06:59 +0000

    container config done

--- FindNumbersWithEvenNumberOfDigits.java
+++ FindNumbersWithEvenNumberOfDigits.java
@@ -3,3 +3,7 @@ class Solution {
     public int findNumbers(int[] nums) {
         int count = 0;
         int even = 0;
+        for(int i = 0; i<nums.length; i++){
+            count = 0;
+            while(nums[i] > 0){
+                nums[i] = nums[i]/10;

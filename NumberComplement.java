e916ba607bca6822180c00930cc55c1a530ac369
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-14 07:46:42 +0000

    container config done

--- NumberComplement.java
+++ NumberComplement.java
@@ -1,1 +1,8 @@
 //--------------------Number Complement-----------------
+class Solution {
+    public int findComplement(int num) {
+        String str = Integer.toBinaryString(num);
+        String res = "";
+
+        for(int i = 0; i<str.length(); i++){
+            if(str.charAt(i) == '0'){

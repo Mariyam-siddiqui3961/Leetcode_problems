642b9b52159d7ce8b4d6825f743a6fe81d78562d
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-15 08:38:37 +0000

    container config done

--- ReverseString.java
+++ ReverseString.java
@@ -3,3 +3,7 @@ class Solution {
     public String reverseStr(String s, int k) {
         char[] arr = s.toCharArray();
         for(int i = 0; i<arr.length; i += 2*k){
+            int left = i;
+            int right = Math.min(i+k-1, arr.length-1);
+
+            while(left < right){

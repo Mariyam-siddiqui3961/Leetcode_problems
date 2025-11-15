0f63d01f48f0b68e28aeaac1ef6d6cad382053ec
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-15 08:38:04 +0000

    container config done

--- ReverseString.java
+++ ReverseString.java
@@ -1,1 +1,5 @@
 //--------reverse string-----------
+class Solution {
+    public String reverseStr(String s, int k) {
+        char[] arr = s.toCharArray();
+        for(int i = 0; i<arr.length; i += 2*k){

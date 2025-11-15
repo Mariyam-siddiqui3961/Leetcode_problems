c1dd0417715703d96c23dbc4259ca60a7d896302
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-15 16:18:39 +0000

    container config done

--- FindTheIndexOfTheFirstOccurance.java
+++ FindTheIndexOfTheFirstOccurance.java
@@ -3,3 +3,6 @@ class Solution {
     public int strStr(String haystack, String needle) {
         int n = haystack.length();
         int m = needle.length();
+
+        for(int i = 0; i<= n-m; i++){
+            int j = 0;

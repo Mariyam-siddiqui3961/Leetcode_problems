f3111c6c6fdcb5565ee80e3dce1720d17657e12a
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-15 16:18:13 +0000

    container config done

--- FindTheIndexOfTheFirstOccurance.java
+++ FindTheIndexOfTheFirstOccurance.java
@@ -1,1 +1,5 @@
 //--------------Find the index of the occurance----------------
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();

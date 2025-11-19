2056fa8d7b2947d22a6628d8f047d6a1c2233dd8
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-19 15:35:59 +0000

    container config done

--- WordBreak.java
+++ WordBreak.java
@@ -1,1 +1,5 @@
 //------------word break------------
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set <String> wordSet = new HashSet<>(wordDict);
+        int n = s.length();

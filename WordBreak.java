708b9b48eedbd7b1aa1951a1fe7c6db872e7b908
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-19 15:36:26 +0000

    container config done

--- WordBreak.java
+++ WordBreak.java
@@ -3,3 +3,8 @@ class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         Set <String> wordSet = new HashSet<>(wordDict);
         int n = s.length();
+        boolean [] dp = new boolean [ n + 1];
+        dp[0] = true;
+
+        for(int i = 1; i<=n; i++){
+            for(int j = 0; j<i; j++){

46e1c14517f88046901914d269446baa6d5b75f3
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-20 15:56:51 +0000

    container config done

--- SplitLinkedList.java
+++ SplitLinkedList.java
@@ -1,1 +1,8 @@
 //-------------Split linked list in parts-----------
+class Solution {
+    public ListNode[] splitListToParts(ListNode head, int k) {
+
+        ListNode[] result = new ListNode[k];
+
+        // Step 1: Count the length of the linked list
+        int length = 0;

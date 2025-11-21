0f1dfec6826ea6277867ba8ea38f8a132307d3ea
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-21 15:34:30 +0000

    container config done

--- TargetSum.java
+++ TargetSum.java
@@ -2,3 +2,13 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int sum = 0;
+
+        for(int num : nums) sum += num;
+
+        if((target + sum) % 2 != 0 || target > sum ) return 0;
+
+        int s = (target + sum) / 2;
+        if(s < 0) return 0;
+
+        int [] dp = new int[s + 1];
+        dp[0] = 1;

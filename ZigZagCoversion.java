922b4208a9cdd81d7f5bc56716bf94bdead1f3d2
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-22 07:58:28 +0000

    container config done

--- ZigZagCoversion.java
+++ ZigZagCoversion.java
@@ -1,1 +1,6 @@
 //---------------zigzag conversion-----------
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1 || s.length() <= numRows)
+            return s;
+        //create rows

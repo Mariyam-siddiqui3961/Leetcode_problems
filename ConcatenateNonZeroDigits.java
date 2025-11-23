b0cda2567ab29363d795d9c4325b909308544db1
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-23 11:51:56 +0000

    container config done

--- ConcatenateNonZeroDigits.java
+++ ConcatenateNonZeroDigits.java
@@ -3,3 +3,9 @@ class Solution {
     public long sumAndMultiply(int n) {
         long x = 0L;
         int sum = 0;
+
+        char[] digits = String.valueOf(n).toCharArray();
+        
+        for (int i = 0; i< digits.length; i++){
+            if(digits[i] == '0') continue;
+                int d = digits[i] - '0';

88b578160901912ad9e45aecadf7ad870120104a
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-24 15:33:54 +0000

    container config done

--- ShuffleArray.java
+++ ShuffleArray.java
@@ -6,3 +6,10 @@ int [] result = new int[2 * n];
         for(int i = 0; i< n; i++){
             result[index] = nums[i];
             index += 1;
+
+            result[index] = nums[i + n];
+            index += 1;
+        }
+        return result;
+    }
+}

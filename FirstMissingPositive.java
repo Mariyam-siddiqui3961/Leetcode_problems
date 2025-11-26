39efda8560a5086fef65b0457674fee0f7814dc1
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-26 09:56:34 +0000

    container config done

--- FirstMissingPositive.java
+++ FirstMissingPositive.java
@@ -10,3 +10,12 @@ class Solution {
                 int temp = nums[i];
                 nums[i] = nums[correctIndex];
                 nums[correctIndex] = temp;
+                }
+        }
+
+        // Step 2: Find first index where value != index+1
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;  // missing positive number
+            }
+        }

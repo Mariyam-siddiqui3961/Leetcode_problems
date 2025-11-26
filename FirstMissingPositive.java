b018f27cdfa90cb1375884686597dc6f13ece037
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-26 09:56:03 +0000

    container config done

--- FirstMissingPositive.java
+++ FirstMissingPositive.java
@@ -2,3 +2,11 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
+        // Step 1: Put numbers in correct position if possible
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                // swap nums[i] with nums[nums[i]-1]
+                int correctIndex = nums[i] - 1;
+                int temp = nums[i];
+                nums[i] = nums[correctIndex];
+                nums[correctIndex] = temp;

8744eba944ece020605c857d8de4803c94c73c84
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-27 15:43:10 +0000

    container config done

--- LetterCombinationOfPhoneNumber.java
+++ LetterCombinationOfPhoneNumber.java
@@ -1,1 +1,11 @@
 //--------------letter combination of phone number---------------
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+
+        if (digits.length() == 0) return result;
+
+        String[] map = {
+                "", "", "abc", "def", "ghi", "jkl",
+                "mno", "pqrs", "tuv", "wxyz"
+        };

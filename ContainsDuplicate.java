4f663c08040a14efa985f61aa3f32a56f564d6e5
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-29 12:23:07 +0000

    container config done

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -16,15 +16,38 @@
 
 
 
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         Arrays.sort(nums); // sort the array
+
+//         for (int i = 1; i < nums.length; i++) {
+//             if (nums[i] == nums[i - 1]) {
+//                 return true;   // found duplicate
+//             }
+//         }
+//         return false; // no duplicate found
+//     }
+// }
+
+
+
+
+
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums); // sort the array
+        int n = nums.length;
+        int left = 0;
+        int right= 0;
 
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {
-                return true;   // found duplicate
+        for(int i = 0; i<=n; i++){
+            if(nums[left] != nums[right]){
+                left++;
+                return true;
+            }else{
+                right++;
             }
         }
-        return false; // no duplicate found
+        return false;
     }
 }

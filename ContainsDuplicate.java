c110f41576d1f7786f8d11fd1c8075155e4bbd7f
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-11-29 12:21:26 +0000

    container config done

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -1,14 +1,30 @@
 //------------Contains Duplicates-------------------
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//          HashSet<Integer> set = new HashSet<>();
+
+//         for (int num: nums){
+//             if (set.contains(num)){
+//                 return true;
+//                 }
+//             set.add(num);
+//             }
+//         return false;
+//     }
+// }
+
+
+
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-         HashSet<Integer> set = new HashSet<>();
+        Arrays.sort(nums); // sort the array
 
-        for (int num: nums){
-            if (set.contains(num)){
-                return true;
-                }
-            set.add(num);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;   // found duplicate
             }
-        return false;
+        }
+        return false; // no duplicate found
     }
 }

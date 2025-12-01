072f4d2e149d328058a3816ab834d513f561c4c9
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-01 16:39:27 +0000

    container config done

--- SearchInRotatesSortedArray.java
+++ SearchInRotatesSortedArray.java
@@ -12,3 +12,12 @@ class Solution {
                 if (target >= nums[left] && target < nums[mid]) {
                     right = mid - 1;
                 } else {
+                    left = mid + 1;
+                }
+            } 
+            // Right sorted part
+            else {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mi

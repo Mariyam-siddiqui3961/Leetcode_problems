b3e9bdf710d5d649fd0377b957ffed87bfcc5f24
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-04 15:14:25 +0000

    container config done

--- TransposeOfMatrix.java
+++ TransposeOfMatrix.java
@@ -1,1 +1,5 @@
 //----------------transpose of matrix----------------
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;

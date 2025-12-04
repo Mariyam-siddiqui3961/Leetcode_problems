d19dfb55a8dae5b5cf96089bdc816c36838fcc74
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-04 15:14:53 +0000

    container config done

--- TransposeOfMatrix.java
+++ TransposeOfMatrix.java
@@ -3,3 +3,9 @@ class Solution {
     public int[][] transpose(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
+
+        nt [][] result = new int[m][n];
+        for(int i = 0; i< n; i++){
+            for(int j = 0; j< m; j++){
+                result[j][i] = matrix[i][j];
+            }

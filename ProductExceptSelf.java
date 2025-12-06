3aeaecc71729ea88aaf441a431b41dd8f33fec6b
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-06 16:54:10 +0000

    container config done

--- ProductExceptSelf.java
+++ ProductExceptSelf.java
@@ -1,1 +1,5 @@
 //----------------product of  array except self------------
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];

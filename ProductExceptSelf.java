fcccd882355d96b01dd5eb87aaeb7ca411110eba
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-06 16:55:23 +0000

    container config done

--- ProductExceptSelf.java
+++ ProductExceptSelf.java
@@ -9,3 +9,9 @@ class Solution {
         for (int i = 1; i < n; i++) {
             ans[i] = ans[i - 1] * nums[i - 1];
         }
+        // Right pass → multiply product of elements after i
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] *= right;
+            right *= nums[i];
+        }

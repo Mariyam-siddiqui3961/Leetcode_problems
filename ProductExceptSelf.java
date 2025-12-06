04b0978ee02df47495c0d164f192e098c0b18faf
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-06 16:54:42 +0000

    container config done

--- ProductExceptSelf.java
+++ ProductExceptSelf.java
@@ -3,3 +3,9 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] ans = new int[n];
+
+        // Left pass → store product of elements before i
+        ans[0] = 1;
+        for (int i = 1; i < n; i++) {
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }

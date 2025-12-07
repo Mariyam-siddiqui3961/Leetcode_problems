12375d0f206f341a904a6f92396751c6855b4720
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-07 03:15:35 +0000

    container config done

--- SortIntegerByBinaryReflection.java
+++ SortIntegerByBinaryReflection.java
@@ -1,1 +1,7 @@
 //---------------sort integer by binery reflection-------------
+class Solution {
+    public int[] sortByReflection(int[] nums) {
+        int [][] arr = new int [nums.length][2];
+
+        for(int i = 0; i< nums.length; i++){
+            int num = nums[i];©leetcode

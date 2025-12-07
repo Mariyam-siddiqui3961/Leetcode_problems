4928eb9cf67de03dbdc49d96c9579fa96a638204
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-07 03:16:35 +0000

    container config done

--- SortIntegerByBinaryReflection.java
+++ SortIntegerByBinaryReflection.java
@@ -4,4 +4,11 @@ class Solution {
         int [][] arr = new int [nums.length][2];
 
         for(int i = 0; i< nums.length; i++){
-            int num = nums[i];©leetcode
+            int num = nums[i];
+            String bin = Integer.toBinaryString(num);
+            String rev = new StringBuilder(bin).reverse().toString();
+            int reflected = Integer.parseInt(rev, 2);
+
+            arr[i][0]= num;
+            arr[i][1] = reflected;
+        }

9d98be6281891c822cba16e1d74ca16528576d7d
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-09 16:49:06 +0000

    container config done

--- ValidPalindrome.java
+++ ValidPalindrome.java
@@ -1,1 +1,6 @@
 //--------------Valid Palindrome--------------
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int left = 0;
+        int right = s.length() -1;

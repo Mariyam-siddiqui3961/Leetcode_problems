97164f6a7fbad16dd81a2c89ca28768efe975865
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-12 15:38:19 +0000

    container config done

--- Power(x,n).java
+++ Power(x,n).java
@@ -1,1 +1,12 @@
 //------------power (x,n)----------------
+class Solution {
+    public double myPow(double x, int n) {
+
+        // If power is negative, invert x and convert n to positive
+        long pow = n; 
+        if (pow < 0) {
+            x = 1 / x;
+            pow = -pow;
+        }
+
+        double result = 1;

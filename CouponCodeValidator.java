4d952da3f6c66e2c92114fc97f04a9a8d3609d57
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-13 16:39:05 +0000

    container config done

--- CouponCodeValidator.java
+++ CouponCodeValidator.java
@@ -5,3 +5,12 @@ class Solution {
 
         // Order of business lines
         String[] order = {"electronics", "grocery", "pharmacy", "restaurant"};
+
+        // Map business line to priority number
+        Map<String, Integer> priority = new HashMap<>();
+        for (int i = 0; i < order.length; i++) {
+            priority.put(order[i], i);
+        }
+
+        // List to store valid coupons
+        List<String[]> valid = new ArrayList<>();

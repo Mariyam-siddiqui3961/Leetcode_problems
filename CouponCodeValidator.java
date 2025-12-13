e4b4483d6e51160f04019411234fb102296ce7ee
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-13 16:38:31 +0000

    container config done

--- CouponCodeValidator.java
+++ CouponCodeValidator.java
@@ -1,1 +1,7 @@
 //------------coupon code validator--------------
+class Solution {
+    public List<String> validateCoupons(
+            String[] code, String[] businessLine, boolean[] isActive) {
+
+        // Order of business lines
+        String[] order = {"electronics", "grocery", "pharmacy", "restaurant"};

d3c940050402c42cb39e782d5dd176a1caadcefb
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-14 12:33:34 +0000

    container config done

--- WaysToDivideLongCorridor.java
+++ WaysToDivideLongCorridor.java
@@ -1,1 +1,5 @@
 //------------number of ways to divide long corridor-------------
+class Solution {
+    public int numberOfWays(String corridor) {
+        int MOD = 1_000_000_007;
+        int seats = 0;

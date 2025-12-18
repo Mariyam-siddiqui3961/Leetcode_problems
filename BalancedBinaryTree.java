33db8bd12b97a7dfb08eece0cdf766c6b937c6aa
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-18 17:49:03 +0000

    container config done

--- BalancedBinaryTree.java
+++ BalancedBinaryTree.java
@@ -6,3 +6,9 @@ class Solution {
 
     private int height(TreeNode node) {
         if (node == null) return 0;
+
+        int left = height(node.left);
+        if (left == -1) return -1;
+
+        int right = height(node.right);
+        if (right == -1) return -1;

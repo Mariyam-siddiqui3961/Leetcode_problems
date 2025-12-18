220ddfdbeff5c03fd8fb487e2802bd40c94a2d13
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-18 17:46:48 +0000

    container config done

--- BalancedBinaryTree.java
+++ BalancedBinaryTree.java
@@ -1,1 +1,8 @@
 //-------------Balanced Binary tree-------------
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return height(root) != -1;
+    }
+
+    private int height(TreeNode node) {
+        if (node == null) return 0;

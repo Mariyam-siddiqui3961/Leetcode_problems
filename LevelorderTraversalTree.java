a89522bbaf5de6b135d7a520f75553eefe931592
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-20 14:12:28 +0000

    container config done

--- LevelorderTraversalTree.java
+++ LevelorderTraversalTree.java
@@ -1,1 +1,8 @@
 //----------level order traversal on tree-----------
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null) return result;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);

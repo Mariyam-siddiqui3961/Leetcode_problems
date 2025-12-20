c531fbd20e2ec5b7460d103b634ccccf0e5066a1
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-20 14:12:55 +0000

    container config done

--- LevelorderTraversalTree.java
+++ LevelorderTraversalTree.java
@@ -6,3 +6,11 @@ class Solution {
 
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            for(int i = 0; i<size; i++){
+                TreeNode node = q.poll();
+                level.add(node.val);
+                if(node.left!= null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);

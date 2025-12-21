ffaabbe18202e23b09e79167d69d26193222912a
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-21 13:03:45 +0000

    container config done

--- DiameterOfBinaryTree.java
+++ DiameterOfBinaryTree.java
@@ -1,1 +1,7 @@
 //-----------diameter of binary tree----------
+class Solution {
+    int diameter = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return diameter;
+    }

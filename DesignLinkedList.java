514acebf7108f1710b99d6c9901fb7e2d023f16d
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-22 16:09:03 +0000

    container config done

--- DesignLinkedList.java
+++ DesignLinkedList.java
@@ -1,1 +1,12 @@
 //--------------------Design Linked List-------------------------
+class MyLinkedList {
+
+    // Node definition
+    private class Node {
+        int val;
+        Node next;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }

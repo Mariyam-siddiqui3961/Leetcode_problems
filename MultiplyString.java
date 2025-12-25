bdc408b5db46e6c16fa62da31802769e101ca230
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-25 15:43:25 +0000

    container config done

--- MultiplyString.java
+++ MultiplyString.java
@@ -1,1 +1,6 @@
 //----------------------------Multiply String-------------------------
+public class Solution {
+    public String multiply(String num1, String num2) {
+        int m = num1.length();
+        int n = num2.length();
+        int[] result = new int[m + n];

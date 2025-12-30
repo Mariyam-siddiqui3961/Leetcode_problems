21926192a03c1238259f5733a275131c17d476bf
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-30 15:50:51 +0000

    container config done

--- RomanToInteger.java
+++ RomanToInteger.java
@@ -1,1 +1,7 @@
 //-----------------Roman to Integer------------------------
+class Solution {
+    public int romanToInt(String s) {
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int curr = value(s.charAt(i));

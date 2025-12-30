b2b227638f789b6d156a977b8d2dcdbf25700c62
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-30 15:51:37 +0000

    container config done

--- RomanToInteger.java
+++ RomanToInteger.java
@@ -5,3 +5,13 @@ class Solution {
 
         for (int i = 0; i < s.length(); i++) {
             int curr = value(s.charAt(i));
+            // If next symbol exists and is larger, subtract
+            if (i + 1 < s.length() && curr < value(s.charAt(i + 1))) {
+                result -= curr;
+            } else {
+                result += curr;
+            }
+        }
+
+        return result;
+    }

986817393acb0e995be09a805d08ed4770b4d11b
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-12-31 14:59:19 +0000

    container config done

--- LastDayWhereYouCanStillCross.java
+++ LastDayWhereYouCanStillCross.java
@@ -1,1 +1,12 @@
 //-----------------------Last day where you can still cross----------------------
+class Solution {
+
+    private int ROWS, COLS;
+    private int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    public int latestDayToCross(int row, int col, int[][] cells) {
+        this.ROWS = row;
+        this.COLS = col;
+
+        int left = 1, right = cells.length;
+        int answer = 0;

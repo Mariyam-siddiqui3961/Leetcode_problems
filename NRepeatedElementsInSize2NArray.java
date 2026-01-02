4fe7d05265ae6c52985cb488c54930065e315112
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-02 08:10:41 +0000

    container config done

--- NRepeatedElementsInSize2NArray.java
+++ NRepeatedElementsInSize2NArray.java
@@ -3,3 +3,7 @@ class Solution {
     public int repeatedNTimes(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
         for(int num: nums){
+            if(set.contains(num)){
+                return num;
+            }
+            set.add(num);

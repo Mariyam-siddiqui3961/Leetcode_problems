e9936f0b1d73c321d88747d3e37e2f4d01338847
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-03 07:38:17 +0000

    container config done

--- NumberOfWaysToPaintGrid.java
+++ NumberOfWaysToPaintGrid.java
@@ -4,3 +4,10 @@ class Solution {
         final int MOD = 1_000_000_007;
         long same = 6;
         long diff = 6;
+        for(int i = 2; i<=n; i++){
+            long newSame = (same * 3 + diff *2) %MOD;
+            long newDiff = (same *2 +diff *2) %MOD;
+
+            same = newSame;
+            diff = newDiff;
+        }

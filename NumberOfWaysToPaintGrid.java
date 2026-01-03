fc414657ef977a4acded1148b552ca8643348be1
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-03 07:37:51 +0000

    container config done

--- NumberOfWaysToPaintGrid.java
+++ NumberOfWaysToPaintGrid.java
@@ -1,1 +1,6 @@
 //-------------------Number of ways to paint grid-------------------
+class Solution {
+    public int numOfWays(int n) {
+        final int MOD = 1_000_000_007;
+        long same = 6;
+        long diff = 6;

6af7a18ecdd06f09d0acd3214390ffb74b032bc3
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-04 09:55:52 +0000

    container config done

--- FourDivisors.java
+++ FourDivisors.java
@@ -5,3 +5,15 @@ class Solution {
         for(int num: nums){
             int count = 0;
             int sum = 0;
+             for(int i = 1; i*i <= num; i++){
+                if(num % i ==0){
+                    int d1 = i;
+                    int d2 = num/i;
+
+                    if (d1 == d2){
+                        count+=1;
+                        sum+=d1;
+                    }else{
+                        count +=2;
+                        sum += d1 + d2;
+                    }

b68764f54ad595177c8a30b8ae8c4b51c0efb966
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-04 09:55:11 +0000

    container config done

--- FourDivisors.java
+++ FourDivisors.java
@@ -1,1 +1,7 @@
 //----------------four divisors------------------------
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        int totalSum = 0;
+        for(int num: nums){
+            int count = 0;
+            int sum = 0;

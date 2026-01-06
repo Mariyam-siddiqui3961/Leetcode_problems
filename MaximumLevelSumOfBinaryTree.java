9b4541c91b0d26184521ba6e657012ac298c9927
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-06 14:19:31 +0000

    container config done

--- MaximumLevelSumOfBinaryTree.java
+++ MaximumLevelSumOfBinaryTree.java
@@ -1,1 +1,8 @@
 //----------------------Maximum Level Sum of a Binary Tree------------------------
+class Solution {
+    public int maxLevelSum(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        int level = 0;
+        int answerLevel = 1;

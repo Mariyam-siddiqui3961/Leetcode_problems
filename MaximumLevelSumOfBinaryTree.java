1e7e1c4a4a825fc5630139347bfb1280d4f18b56
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-06 14:20:04 +0000

    container config done

--- MaximumLevelSumOfBinaryTree.java
+++ MaximumLevelSumOfBinaryTree.java
@@ -6,3 +6,13 @@ class Solution {
 
         int level = 0;
         int answerLevel = 1;
+        long maxSum = Long.MIN_VALUE;
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            level++;
+            long levelSum = 0;
+
+            for(int i = 0; i<size; i++){
+                TreeNode node = q.poll();
+                levelSum += node.val;

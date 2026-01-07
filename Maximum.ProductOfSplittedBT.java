83811ab6dfb12ca05f785717573e18a18660e2f7
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-07 16:11:13 +0000

    container config done

--- Maximum.ProductOfSplittedBT.java
+++ Maximum.ProductOfSplittedBT.java
@@ -13,3 +13,16 @@ class Solution {
 
         return (int)(maxProduct % MOD);
     }
+    // DFS to compute total sum
+    private long treeSum(TreeNode node) {
+        if (node == null) return 0;
+        return node.val + treeSum(node.left) + treeSum(node.right);
+    }
+
+    // DFS to compute subtree sums and products
+    private long computeProduct(TreeNode node) {
+        if (node == null) return 0;
+
+        long leftSum = computeProduct(node.left);
+        long rightSum = computeProduct(node.right);
+

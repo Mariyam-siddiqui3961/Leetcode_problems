fd503076e6ca3b32fb4ab9ca3a60a3b0dafd109a
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-07 16:10:40 +0000

    container config done

--- Maximum.ProductOfSplittedBT.java
+++ Maximum.ProductOfSplittedBT.java
@@ -1,1 +1,15 @@
 //---------------------Maximum product of splitted binary tree--------------------------
+class Solution {
+    private long totalSum = 0;
+    private long maxProduct = 0;
+    private static final int MOD = 1_000_000_007;
+
+    public int maxProduct(TreeNode root) {
+        // Step 1: compute total sum
+        totalSum = treeSum(root);
+
+        // Step 2: compute max product
+        computeProduct(root);
+
+        return (int)(maxProduct % MOD);
+    }

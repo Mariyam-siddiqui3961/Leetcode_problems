487038302e9ec5956973732bbabc20836e931617
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-08 14:02:06 +0000

    container config done

--- MaxDotProductOfTwoSubsequences.java
+++ MaxDotProductOfTwoSubsequences.java
@@ -1,1 +1,14 @@
 //-----------------Max dot product of two subsequences------------------
+class Solution {
+    public int maxDotProduct(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        // Initialize with very small values
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                dp[i][j] = Integer.MIN_VALUE;
+            }
+        }

44ef334e7d29906a43af922a02955b8a7c3e7f8f
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-09 05:41:00 +0000

    container config done

--- SmallestSubtreeWithDeepestNode.java
+++ SmallestSubtreeWithDeepestNode.java
@@ -1,1 +1,12 @@
 //--------------------smallest subtree with all the deepest node-------------------
+class Solution {
+
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        if (root == null) return null;
+
+        int leftDepth = depth(root.left);
+        int rightDepth = depth(root.right);
+
+        if (leftDepth == rightDepth) {
+            return root;
+        }

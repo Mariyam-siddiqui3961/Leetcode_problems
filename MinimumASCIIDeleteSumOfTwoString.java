bced6b4106541fe37bc5a9940d97f6f4b05bbfde
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-10 16:46:19 +0000

    container config done

--- MinimumASCIIDeleteSumOfTwoString.java
+++ MinimumASCIIDeleteSumOfTwoString.java
@@ -5,3 +5,13 @@ class Solution {
         int m = s2.length();
 
         int[][] dp = new int[n + 1][m + 1];
+        // Build DP table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + s1.charAt(i - 1);
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }

2d7e9fc71dc830fdc218b5a55a52167c7ebae726
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-11 12:57:29 +0000

    container config done

--- MaximalRectangle.java
+++ MaximalRectangle.java
@@ -1,1 +1,7 @@
 //-------------------Maximal Rectangle---------------------
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix == null || matrix.length == 0) return 0;
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;

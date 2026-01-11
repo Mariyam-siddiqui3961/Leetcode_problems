5c3f6a2cce63a81305849cf88786d3388d2d58d2
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-11 12:58:13 +0000

    container config done

--- MaximalRectangle.java
+++ MaximalRectangle.java
@@ -5,3 +5,25 @@ class Solution {
 
         int rows = matrix.length;
         int cols = matrix[0].length;
+
+        int[] heights = new int[cols];
+        int maxArea = 0;
+
+        // Process each row as a histogram
+        for (int r = 0; r < rows; r++) {
+
+            // Update heights
+            for (int c = 0; c < cols; c++) {
+                if (matrix[r][c] == '1') {
+                    heights[c]++;
+                } else {
+                    heights[c] = 0;
+                }
+            }
+
+            // Calculate max area for this histogram
+            maxArea = Math.max(maxArea, largestRectangleArea(heights));
+        }
+
+        return maxArea;
+    }

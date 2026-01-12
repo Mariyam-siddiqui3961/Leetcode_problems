06653f3e6c0a2e2173b24bf406ad9a71f19908a3
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-12 05:29:27 +0000

    container config done

--- LongestCommonSubsequences.java
+++ LongestCommonSubsequences.java
@@ -1,1 +1,7 @@
 //-------------longest common subsequences----------------
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+
+        int dp[][] = new int[m+1][n+1];

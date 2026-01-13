47bd53fcaf330b7dc954376711c859e92e3ca14c
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-13 09:30:11 +0000

    container config done

--- UniquePath.java
+++ UniquePath.java
@@ -1,1 +1,6 @@
 //-----------Unique Path-----------------
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int [][]dp = new int [m][n];
+        for(int i = 0; i<m; i++) dp [i][0] = 1;
+        for(int j = 0; j <n; j++) dp[0][j] = 1;

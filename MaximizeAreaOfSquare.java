c8abceba8a9d4c51e08a729dec2f588fec5bcacc
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-15 16:22:10 +0000

    container config done

--- MaximizeAreaOfSquare.java
+++ MaximizeAreaOfSquare.java
@@ -1,1 +1,30 @@
 //-------------------Maximize Area of square hole in grid-----------------
+class Solution {
+    public int maximizeSquareHoleArea(int n, int m, int[] hBars, int[] vBars) {
+
+        int maxHeight = getMaxGap(hBars);
+        int maxWidth = getMaxGap(vBars);
+
+        int side = Math.min(maxHeight, maxWidth);
+        return side * side;
+    }
+
+    private int getMaxGap(int[] bars) {
+        Arrays.sort(bars);
+
+        int maxConsecutive = 1;
+        int current = 1;
+
+        for (int i = 1; i < bars.length; i++) {
+            if (bars[i] == bars[i - 1] + 1) {
+                current++;
+            } else {
+                current = 1;
+            }
+            maxConsecutive = Math.max(maxConsecutive, current);
+        }
+
+        // +1 because removing k consecutive bars creates (k+1) gap
+        return maxConsecutive + 1;
+    }
+}

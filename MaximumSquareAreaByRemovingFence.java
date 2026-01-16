980072028ba232739fd6dbe656edaeeb407ff6e2
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-16 12:57:17 +0000

    container config done

--- MaximumSquareAreaByRemovingFence.java
+++ MaximumSquareAreaByRemovingFence.java
@@ -1,1 +1,8 @@
 //-----------maximum square area by removing fence----------------
+class Solution {
+    public int maximizeSquareArea(int m, int n, int[] hFences, int[] vFences) {
+        final int MOD = 1_000_000_007;
+
+        // Add boundaries
+        int[] H = new int[hFences.length + 2];
+        int[] V = new int[vFences.length + 2];

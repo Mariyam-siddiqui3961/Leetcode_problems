678cace92f2236ce9af36ea16e468161dd5426f6
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-17 13:34:40 +0000

    container config done

--- CoinChange.java
+++ CoinChange.java
@@ -1,1 +1,8 @@
 //-------------------coin change---------------
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int [] dp = new int [amount +1];
+
+        for(int i = 0; i<= amount; i++){
+            dp[i] = amount +1;
+        }

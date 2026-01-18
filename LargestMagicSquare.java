d6dcf52926b270177f0a9871379221179555a81e
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-18 04:47:05 +0000

    container config done

--- LargestMagicSquare.java
+++ LargestMagicSquare.java
@@ -1,1 +1,23 @@
 //--------------largest magic square-------------------
+class Solution {
+    public int largestMagicSquare(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+
+        // Prefix sums
+        int[][] row = new int[m][n + 1];
+        int[][] col = new int[m + 1][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                row[i][j + 1] = row[i][j] + grid[i][j];
+                col[i + 1][j] = col[i][j] + grid[i][j];
+            }
+        }
+
+        // Try sizes from largest to smallest
+        for (int k = Math.min(m, n); k >= 2; k--) {
+            for (int r = 0; r + k <= m; r++) {
+                for (int c = 0; c + k <= n; c++) {
+                    if (isMagic(grid, row, col, r, c, k)) {
+                        return k;
+                    }

eaadecc49f1d4036f3fae6b5e3141144991885fd
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-19 14:46:47 +0000

    container config done

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -1,16 +1,29 @@
 //---------Climbing Stairs--------------------
+// class Solution {
+//     public int climbStairs(int n) {
+//         if (n <= 2) return n;
+
+//         int a = 1; // ways to reach step 1
+//         int b = 2; // ways to reach step 2
+
+//         for (int i = 3; i <= n; i++) {
+//             int c = a + b;
+//             a = b;
+//             b = c;
+//         }
+//         return b;
+//     }
+// }
+
+
+
+
+
 class Solution {
     public int climbStairs(int n) {
-        if (n <= 2) return n;
 
-        int a = 1; // ways to reach step 1
-        int b = 2; // ways to reach step 2
+        if (n <= 1) return n;
 
-        for (int i = 3; i <= n; i++) {
-            int c = a + b;
-            a = b;
-            b = c;
-        }
-        return b;
-    }
-}
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;

cae5c79be40f9b491fde0663b7e7e6cd4c5d3d51
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-20 14:25:33 +0000

    container config done

--- PartitionEqualSubsetSum.java
+++ PartitionEqualSubsetSum.java
@@ -1,1 +1,10 @@
 //-----------partition equal subset sum ------------
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for(int num: nums){
+            sum += num;
+        }
+        if (sum %2 != 0) return false;
+
+        int target = sum/2;

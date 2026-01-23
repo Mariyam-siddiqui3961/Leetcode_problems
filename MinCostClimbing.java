c9fa739b6f14446ea8097798f8ac145495a59eab
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-23 15:22:43 +0000

    container config done

--- MinCostClimbing.java
+++ MinCostClimbing.java
@@ -1,1 +1,7 @@
 //--------------Min cost of climibing stairs------------------
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;        
+        int [] dp = new int [n];
+        dp[0] = cost[0];
+        dp[1] = cost[1];

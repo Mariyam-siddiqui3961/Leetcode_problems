0b4e0932d04815de41c0698de5f5fc8539c16060
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-24 09:05:14 +0000

    container config done

--- HouseRobber.java
+++ HouseRobber.java
@@ -1,1 +1,8 @@
 //-----------------------House robber------------------------
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        if(n == 1) return nums[0];
+
+        int [] dp = new int [n];

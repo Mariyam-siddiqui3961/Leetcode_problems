6cd73c0971a2bf1d6a670aa55856b08a598244e5
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-25 07:18:09 +0000

    container config done

--- MinDiffBitweenHighestAndLowestKScore.java
+++ MinDiffBitweenHighestAndLowestKScore.java
@@ -5,3 +5,13 @@ class Solution {
 
         if (k == 1) return 0;
         Arrays.sort(nums);
+
+        int minDiff = Integer.MAX_VALUE;
+
+        for(int i =0; i+k-1 < n; i++){
+            int diff = nums[i+k-1] - nums[i];
+            minDiff = Math.min(minDiff, diff);
+        }
+        return minDiff;    
+    }
+}

c6802eb39da33a12ad93b5759cc5d70e1283b72b
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-26 05:55:09 +0000

    container config done

--- MinimumAbsoluteDiff.java
+++ MinimumAbsoluteDiff.java
@@ -6,3 +6,19 @@ class Solution {
 
         int minDiff = Integer.MAX_VALUE;
         List<List<Integer>> result = new ArrayList<>();
+
+        // Step 1: Find minimum difference
+        for (int i = 1; i < arr.length; i++) {
+            minDiff = Math.min(minDiff, arr[i] - arr[i - 1]);
+        }
+
+        // Step 2: Collect all pairs with min difference
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - arr[i - 1] == minDiff) {
+                result.add(Arrays.asList(arr[i - 1], arr[i]));
+            }
+        }
+
+        return result;
+    }
+}

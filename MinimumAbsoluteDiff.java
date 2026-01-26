ba7249bf9631cd8cdae56a98ead14b898c16d820
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-26 05:54:21 +0000

    container config done

--- MinimumAbsoluteDiff.java
+++ MinimumAbsoluteDiff.java
@@ -1,1 +1,8 @@
 //--------------minimum absolute diff-------------------
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+
+        Arrays.sort(arr);
+
+        int minDiff = Integer.MAX_VALUE;
+        List<List<Integer>> result = new ArrayList<>();

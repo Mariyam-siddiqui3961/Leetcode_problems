e0333aa2bb4319f341eb088b46bb51ed5c6dc783
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-28 13:30:26 +0000

    container config done

--- SortArrayByParity.java
+++ SortArrayByParity.java
@@ -1,1 +1,11 @@
 //----------------sort array by parity-------------------
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        int idx = 0;
+        for(int num: nums){
+            if(num % 2 == 0){
+                res[idx++] = num;
+            }
+        }

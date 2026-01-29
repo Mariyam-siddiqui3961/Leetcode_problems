e570800bcdeaba130f7e57d50f038a0120d9b5f4
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-29 16:10:18 +0000

    container config done

--- SortColors.java
+++ SortColors.java
@@ -21,3 +21,14 @@ class Solution {
                 idx += 1;
             }
         }
+        for(int num: nums){
+            if(num == blue){
+                res[idx] = num;
+                idx += 1;
+            }
+        }
+        for(int i =0; i<n; i++){
+            nums[i] = res[i];
+        }
+    }
+}

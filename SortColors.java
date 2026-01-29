399bcdc2a7d532857a65b936dbb9d0d4242ccfc9
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-29 16:09:49 +0000

    container config done

--- SortColors.java
+++ SortColors.java
@@ -6,3 +6,18 @@ class Solution {
         int red = 0;
         int white = 1;
         int blue = 2;
+        int[] res = new int [n];
+        int idx =0;
+ 
+        for(int num: nums){
+            if(num == red){
+                res[idx] = num;
+                idx += 1;
+            }
+        }
+        for(int num: nums){
+            if(num == white){
+                res[idx] = num;
+                idx += 1;
+            }
+        }

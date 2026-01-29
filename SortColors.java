c2b8ed308e484ae72b7681968e52026b117ee1e0
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-29 16:09:26 +0000

    container config done

--- SortColors.java
+++ SortColors.java
@@ -1,1 +1,8 @@
 //-------------------sort colors----------------------
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+
+        int red = 0;
+        int white = 1;
+        int blue = 2;

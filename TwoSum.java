645e07973f5395e02c9fad1f8cc63bb80b73a39c
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-30 13:38:50 +0000

    container config done

--- TwoSum.java
+++ TwoSum.java
@@ -1,13 +1,27 @@
 // ----------------------ARRAY----------------->
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map= new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            int comp= target-nums[i];
-            if(map.containsKey(comp))
-                return new int[]{ map.get(comp), i};
-            map.put(nums[i], i);
-         }
-         return new int[0];
+        // Map<Integer, Integer> map= new HashMap<>();
+        // for(int i=0; i<nums.length; i++){
+        //     int comp= target-nums[i];
+        //     if(map.containsKey(comp))
+        //         return new int[]{ map.get(comp), i};
+        //     map.put(nums[i], i);
+        //  }
+        //  return new int[0];
+
+
+
+
+
+        int n = nums.length;
+        for(int i = 0; i<n; i++){
+            for(int j = i+1; j<n; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{-1,-1};
     }
 }

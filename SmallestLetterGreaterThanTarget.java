cef04abc248422e49f0f3f384902cc532f7d16bd
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-31 16:18:34 +0000

    container config done

--- SmallestLetterGreaterThanTarget.java
+++ SmallestLetterGreaterThanTarget.java
@@ -1,1 +1,8 @@
 //----------------------Smallest letter greater than target--------------------
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+
+        int left = 0, right = letters.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;

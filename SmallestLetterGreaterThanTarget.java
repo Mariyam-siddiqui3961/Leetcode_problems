129a824a32a7734335f991cf8a98cf282e589662
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-01-31 16:19:05 +0000

    container config done

--- SmallestLetterGreaterThanTarget.java
+++ SmallestLetterGreaterThanTarget.java
@@ -6,3 +6,13 @@ class Solution {
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return letters[left % letters.length];
+    }
+}

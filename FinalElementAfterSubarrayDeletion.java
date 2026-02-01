55cc5e92af4d132c07bf4c25a3ce78bbf579a32c
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-02-01 03:22:01 +0000

    container config done

--- FinalElementAfterSubarrayDeletion.java
+++ FinalElementAfterSubarrayDeletion.java
@@ -1,1 +1,10 @@
 //---------------final element after subarray deletion----------------
+class Solution {
+    public int finalElement(int[] nums) {
+        int n = nums.length;
+        if(n ==0)return nums[0];
+
+        return Math.max(nums[0], nums[n-1]);
+
+    }
+}

55f704399b0708480467156075e01f8d4ff77e2c
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-02-02 15:40:09 +0000

    container config done

--- SetMisMatch.java
+++ SetMisMatch.java
@@ -1,1 +1,6 @@
 //-------------Set Mismatch----------------------
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        int duplicate = -1;
+        int sum = 0;

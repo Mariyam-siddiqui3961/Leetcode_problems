cb9fb247bb50c7a0a33cd839d588561edcbc7e77
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2026-02-04 17:16:19 +0000

    container config done

--- HighestAltitude.java
+++ HighestAltitude.java
@@ -1,1 +1,5 @@
 //---------------highest altitude----------------------
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int currentAltitude = 0;
+        int maxAltitude = 0;
